0d117a8fc86ae5bef706464bae5ad3a6869675c7
jeongbeomSeo <jeongbeom4693@gmail.com>
2023-04-12 11:19:29 +0900

    Solve Problem Using Greedy Algorithm

--- baekjoon/combination/word-math/Main.java
+++ baekjoon/combination/word-math/Main.java
@@ -1,25 +1,69 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    StringTokenizer st;
 
     int N = Integer.parseInt(br.readLine());
 
-    int[] usingChar = new int['Z' - 'A' + 1];
-    int[] usingNumber = new int[10];
-
+    int[] alphabet = new int['Z' - 'A' + 1];
     String[] str = new String[N];
-    int maxLength = 0;
+
     for (int i = 0; i < N; i++) {
       str[i] = br.readLine();
-      maxLength = Math.max(maxLength, str[i].length());
     }
 
+    transform_string(str, alphabet, N);
+
+    System.out.println(calculateWordNumber(alphabet));
   }
-  static void calcGreedy(String[] str, int[] usingChar, )
+  static private int charToInt(char c) {
+    return c - 'A';
+  }
+
+  static void transform_string(String[] str, int[] alphabet, int N) {
+
+    for (int i = 0; i < N; i++) {
+      int strLength = str[i].length();
+
+      for (int j = 0; j < strLength; j++) {
+        char curChar = str[i].charAt(j);
+        int curIdx = charToInt(curChar);
+
+        alphabet[curIdx] += (int)Math.pow(10, (strLength - 1) - j);
+      }
+    }
+  }
+
+  static int calculateWordNumber(int[] alphabet) {
+
+    int length = 'Z' - 'A' + 1;
+    int sum = 0;
+
+    int num = 9;
+    while (num >= 0) {
+      boolean change = false;
+
+      int maxIdx = -1;
+      int maxValue = 0;
+      for (int i = 0; i < length; i++) {
+        if (maxValue < alphabet[i]) {
+          maxIdx = i;
+          maxValue = alphabet[i];
+          change = true;
+        }
+      }
+
+      if (!change) break;
+
+      sum += (num * alphabet[maxIdx]);
+
+      alphabet[maxIdx] = 0;
+      num--;
+    }
+    return sum;
+
+  }
 }
