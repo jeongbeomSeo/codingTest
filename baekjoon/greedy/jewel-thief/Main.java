d625e242f945178a2ea75bba06daaad064d51f6f
jeongbeomSeo <jeongbeom4693@gmail.com>
2023-05-01 14:37:40 +0900

    Solve Problem Using PriorityQueue and Greedy

--- baekjoon/greedy/jewel-thief/Main.java
+++ baekjoon/greedy/jewel-thief/Main.java
@@ -1,13 +1,9 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-  static int sum = 0;
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     StringTokenizer st = new StringTokenizer(br.readLine());
@@ -15,70 +11,57 @@ public class Main {
     int N = Integer.parseInt(st.nextToken());
     int K = Integer.parseInt(st.nextToken());
 
-    ArrayList<Jewel> jewels = new ArrayList<>();
-    ArrayList<Bag> bags = new ArrayList<>();
+    Jewel[] jewels = new Jewel[N];
+    int[] bags = new int[K];
 
-    for (int i = 0; i < N; i++) {
+    for (int i = 0 ; i < N; i++) {
       st = new StringTokenizer(br.readLine());
       int weight = Integer.parseInt(st.nextToken());
       int cost = Integer.parseInt(st.nextToken());
-      jewels.add(new Jewel(weight, cost));
+      jewels[i] = new Jewel(weight, cost);
     }
 
     for (int i = 0; i < K; i++) {
-      int size = Integer.parseInt(br.readLine());
-      bags.add(new Bag(size));
+      bags[i] = Integer.parseInt(br.readLine());
     }
 
-    Collections.sort(jewels);
-    Collections.sort(bags);
+    Arrays.sort(jewels, new Comparator<Jewel>() {
+      @Override
+      public int compare(Jewel o1, Jewel o2) {
+        if (o1.weight == o2.weight)
+          return o2.cost - o1.cost;
+        return o1.weight - o2.weight;
+      }
+    });
+    Arrays.sort(bags);
 
-    query(jewels, bags, N, K);
-
-    System.out.println(sum);
+    System.out.println(greedy(jewels, bags, N, K));
   }
-  static void query(ArrayList<Jewel> jewels, ArrayList<Bag> bags, int N, int K) {
+  static long greedy(Jewel[] jewels, int[] bags, int N, int K) {
+    Queue<Jewel> pq = new PriorityQueue<>((o1, o2) -> o2.cost - o1.cost);
+    long totalMoney = 0;
 
-    int bagSize = K;
+    int idx = 0;
+    for (int i = 0 ; i < K; i++) {
 
-    for (int i = 0; i < N; i++) {
-      for (int j = 0; j < bagSize; j++) {
-        if (bags.get(j).size >= jewels.get(i).weight) {
-          sum += jewels.get(i).cost;
-          bags.remove(j);
-          bagSize--;
-          break;
-        }
+      while (idx < N && jewels[idx].weight <= bags[i]) {
+        pq.offer(jewels[idx++]);
+      }
+
+      if (!pq.isEmpty()) {
+        totalMoney += pq.poll().cost;
       }
     }
+    return totalMoney;
   }
 }
 
-class Bag implements Comparable<Bag> {
-  int size;
-  int cost;
-
-  Bag(int size) {
-    this.size = size;
-  }
-
-  @Override
-  public int compareTo(Bag o) {
-    return this.size - o.size;
-  }
-}
-
-class Jewel implements Comparable<Jewel>{
+class Jewel {
   int weight;
   int cost;
 
   Jewel(int weight, int cost) {
     this.weight = weight;
     this.cost = cost;
-  }
-
-  @Override
-  public int compareTo(Jewel o) {
-    return o.cost - this.cost;
   }
 }
--- baekjoon/greedy/jewel-thief/out/production/jewel-thief/README.md
+++ baekjoon/greedy/jewel-thief/out/production/jewel-thief/README.md
@@ -151,3 +151,96 @@ class Jewel implements Comparable<Jewel>{
   }
 }
 ```
+
+**TLE**
+
+```java
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class Main {
+  static int sum = 0;
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+
+    int N = Integer.parseInt(st.nextToken());
+    int K = Integer.parseInt(st.nextToken());
+
+    ArrayList<Jewel> jewels = new ArrayList<>();
+    ArrayList<Bag> bags = new ArrayList<>();
+
+    for (int i = 0; i < N; i++) {
+      st = new StringTokenizer(br.readLine());
+      int weight = Integer.parseInt(st.nextToken());
+      int cost = Integer.parseInt(st.nextToken());
+      jewels.add(new Jewel(weight, cost));
+    }
+
+    for (int i = 0; i < K; i++) {
+      int size = Integer.parseInt(br.readLine());
+      bags.add(new Bag(size));
+    }
+
+    Collections.sort(jewels);
+    Collections.sort(bags);
+
+    query(jewels, bags, N, K);
+
+    System.out.println(sum);
+  }
+  static void query(ArrayList<Jewel> jewels, ArrayList<Bag> bags, int N, int K) {
+
+    int bagSize = K;
+
+    for (int i = 0; i < N; i++) {
+      for (int j = 0; j < bagSize; j++) {
+        if (bags.get(j).size >= jewels.get(i).weight) {
+          sum += jewels.get(i).cost;
+          bags.remove(j);
+          bagSize--;
+          break;
+        }
+      }
+    }
+  }
+}
+
+class Bag implements Comparable<Bag> {
+  int size;
+  int cost;
+
+  Bag(int size) {
+    this.size = size;
+  }
+
+  @Override
+  public int compareTo(Bag o) {
+    return this.size - o.size;
+  }
+}
+
+class Jewel implements Comparable<Jewel>{
+  int weight;
+  int cost;
+
+  Jewel(int weight, int cost) {
+    this.weight = weight;
+    this.cost = cost;
+  }
+
+  @Override
+  public int compareTo(Jewel o) {
+    return o.cost - this.cost;
+  }
+}
+```
+
+사용한 가방을 지워줌으로써 반복문에서 효율을 높이려고 했는데 별 효과가 없었다.
+
+아마 정렬하려는 행위 자체가 문제일 수도 있겠다 ...
