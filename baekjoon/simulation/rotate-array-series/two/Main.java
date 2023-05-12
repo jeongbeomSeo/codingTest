26ccbbec4f15549e5f5fb76c352c4cfacefe7ef5
jeongbeomSeo <jeongbeom4693@gmail.com>
2023-05-12 19:23:47 +0900

    Final Solution

--- baekjoon/simulation/rotate-array-series/two/Main.java
+++ baekjoon/simulation/rotate-array-series/two/Main.java
@@ -1,20 +1,18 @@
+import java.awt.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-  static int N, M;
-  // 동, 남, 서, 북
-  static int[] dr = {0, 1, 0, -1};
-  static int[] dc = {1, 0, -1, 0};
-
+  static int N, M, R;
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     StringTokenizer st = new StringTokenizer(br.readLine());
+
     N = Integer.parseInt(st.nextToken());
     M = Integer.parseInt(st.nextToken());
-    int R = Integer.parseInt(st.nextToken());
+    R = Integer.parseInt(st.nextToken());
 
     int[][] grid = new int[N][M];
     for (int i = 0; i < N; i++) {
@@ -24,7 +22,33 @@ public class Main {
       }
     }
 
-    simulation(grid, R);
+    simulation(grid);
+
+  }
+  static void simulation(int[][] baseGrid) {
+
+    int num = 0;
+    int last_row = N - 1;
+    int last_col = M - 1;
+    int[][] grid = new int[N][M];
+    while (last_row - num >= 1 && last_col - num >= 1) {
+      int number_count = (last_row - num) * 2 + (last_col - num) * 2;
+      int rotate = R % number_count;
+
+      Pointer prev_pointer = new Pointer(num, last_row, last_col);
+      for (int i = 0; i < rotate; i++) prev_pointer.move();
+      Pointer pointer = new Pointer(num, last_row, last_col);
+
+      while (number_count-- > 0) {
+        grid[pointer.row][pointer.col] = baseGrid[prev_pointer.row][prev_pointer.col];
+        pointer.move();
+        prev_pointer.move();
+      }
+
+      last_col--;
+      last_row--;
+      num++;
+    }
 
     for (int i = 0; i < N; i++) {
       for (int j = 0; j < M; j++) {
@@ -32,38 +56,44 @@ public class Main {
       }
       System.out.println();
     }
+
   }
-  static void simulation(int[][] grid, int R) {
+}
+class Pointer {
+  private static final int[] dr = {0, 1, 0, -1};
+  private static final int[] dc = {1, 0, -1, 0};
+  private final int R;
+  private final int C;
+  int min_num;
+  int row;
+  int col;
+  int direction;
 
-    int num = 0;
-    int end_row = N - 1;
-    int end_col = M - 1;
+  Pointer(int num, int R, int C) {
+    this.min_num = num;
+    this.row = num;
+    this.col = num;
+    this.R = R;
+    this.C = C;
+    this.direction = 0;
+  }
 
-    while (end_row - num >= 1 && end_col - num >= 1) {
-      int number_count = (end_row - num) * 2 + (end_col - num) * 2;
-      int rotate = R % number_count;
+  void move() {
 
-      while (rotate-- > 0) {
-        int row = num;
-        int col = num;
-        int temp = grid[row][col]; // 6
-        for (int i = 0; i < 4; i++) {
-          while (isValidIdx(row + dr[i], col + dc[i], num, end_row, end_col)) {
-            grid[row][col] = grid[row + dr[i]][col + dc[i]];
-            row += dr[i];
-            col += dc[i];
-          }
-        }
-        grid[row + 1][col] = temp;
-      }
-
-      end_row--;
-      end_col--;
-      num++;
+    int row = this.row + dr[direction];
+    int col = this.col + dc[direction];
+    if (!isValidIdx(row, col)) {
+      direction = (direction + 1) % 4;
+      row = this.row + dr[direction];
+      col = this.col + dc[direction];
     }
 
+    this.row = row;
+    this.col = col;
   }
-  static boolean isValidIdx(int row, int col, int num, int end_row, int end_col) {
-    return row >= num && col >= num && row <= end_row && col <= end_col;
+
+  boolean isValidIdx(int row, int col) {
+    return row >= min_num && col >= min_num && row <= R && col <= C;
   }
+
 }
--- baekjoon/simulation/rotate-array-series/two/README.md
+++ baekjoon/simulation/rotate-array-series/two/README.md
@@ -307,3 +307,107 @@ public class Main {
 
 시간이 1356ms -> 1428 ms로 늘어났습니다.
 메모리는 37900KB -> 35768KB로 줄어들었습니다.
+
+
+**최종 코드**
+**AC**
+```java
+import java.awt.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+  static int N, M, R;
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+
+    N = Integer.parseInt(st.nextToken());
+    M = Integer.parseInt(st.nextToken());
+    R = Integer.parseInt(st.nextToken());
+
+    int[][] grid = new int[N][M];
+    for (int i = 0; i < N; i++) {
+      st = new StringTokenizer(br.readLine());
+      for (int j = 0; j < M; j++) {
+        grid[i][j] = Integer.parseInt(st.nextToken());
+      }
+    }
+
+    simulation(grid);
+
+  }
+  static void simulation(int[][] baseGrid) {
+
+    int num = 0;
+    int last_row = N - 1;
+    int last_col = M - 1;
+    int[][] grid = new int[N][M];
+    while (last_row - num >= 1 && last_col - num >= 1) {
+      int number_count = (last_row - num) * 2 + (last_col - num) * 2;
+      int rotate = R % number_count;
+
+      Pointer prev_pointer = new Pointer(num, last_row, last_col);
+      for (int i = 0; i < rotate; i++) prev_pointer.move();
+      Pointer pointer = new Pointer(num, last_row, last_col);
+
+      while (number_count-- > 0) {
+        grid[pointer.row][pointer.col] = baseGrid[prev_pointer.row][prev_pointer.col];
+        pointer.move();
+        prev_pointer.move();
+      }
+
+      last_col--;
+      last_row--;
+      num++;
+    }
+
+    for (int i = 0; i < N; i++) {
+      for (int j = 0; j < M; j++) {
+        System.out.print(grid[i][j] + " ");
+      }
+      System.out.println();
+    }
+
+  }
+}
+class Pointer {
+  private static final int[] dr = {0, 1, 0, -1};
+  private static final int[] dc = {1, 0, -1, 0};
+  private final int R;
+  private final int C;
+  int min_num;
+  int row;
+  int col;
+  int direction;
+
+  Pointer(int num, int R, int C) {
+    this.min_num = num;
+    this.row = num;
+    this.col = num;
+    this.R = R;
+    this.C = C;
+    this.direction = 0;
+  }
+
+  void move() {
+
+    int row = this.row + dr[direction];
+    int col = this.col + dc[direction];
+    if (!isValidIdx(row, col)) {
+      direction = (direction + 1) % 4;
+      row = this.row + dr[direction];
+      col = this.col + dc[direction];
+    }
+
+    this.row = row;
+    this.col = col;
+  }
+
+  boolean isValidIdx(int row, int col) {
+    return row >= min_num && col >= min_num && row <= R && col <= C;
+  }
+}
+```
