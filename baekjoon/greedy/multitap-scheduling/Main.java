22f3985ac020f27472c959c251bb553da9456452
jeongbeomSeo <jeongbeom4693@gmail.com>
2023-05-21 14:12:29 +0900

    Final Solve Problem

--- baekjoon/greedy/multitap-scheduling/Main.java
+++ baekjoon/greedy/multitap-scheduling/Main.java
@@ -10,64 +10,61 @@ public class Main {
 
     int N = Integer.parseInt(st.nextToken());
     int K = Integer.parseInt(st.nextToken());
-    int[] products = new int[K];
 
+    int[] tap = new int[N];
+    int size = 0;
+    boolean[] isUsing = new boolean[K + 1];
+
+    int[] items = new int[K];
     st = new StringTokenizer(br.readLine());
     for (int i = 0; i < K; i++) {
-      products[i] = Integer.parseInt(st.nextToken());
+      items[i] = Integer.parseInt(st.nextToken());
     }
 
-    System.out.println(greedy(products, N, K));
-
-  }
-  static int greedy(int[] products, int N, int K) {
-
     int count = 0;
-    int[] tap = new int[N];
-    boolean[] using = new boolean[K + 1];
-    int size = 0;
-
     for (int i = 0; i < K; i++) {
-      if (!using[products[i]]) {
-        if (size < N) {
-          tap[size++] = products[i];
-        }
-        else {
-          boolean[] isNextUse = new boolean[N];
-          int nextUseCount = 0;
-          int maxIdx = 0;
-          for (int j = i + 1; j < K; j++) {
-            if (using[products[j]]) {
-              for (int k = 0; k < N; k++) {
-                if (tap[k] == products[j]) {
-                  if (isNextUse[k]) break;
-                  maxIdx = k;
-                  isNextUse[k] = true;
-                  nextUseCount++;
-                  break;
-                }
-              }
-            }
-          }
-          if (nextUseCount == N) {
-            using[tap[maxIdx]] = false;
-            tap[maxIdx] = products[i];
-            count++;
-          }
-          else {
-            for (int j = 0; j < N; j++) {
-              if (!isNextUse[j]) {
-                using[tap[j]] = false;
-                tap[j] = products[i];
-                count++;
+      int item = items[i];
+      if (isUsing[item]) continue;
+
+      if (size < N) tap[size++] = item;
+      else {
+        int nextUsingCount = 0;
+        int[] nextUsingIdx = new int[N];
+        for (int j = i + 1; j < K; j++) {
+          if (isUsing[items[j]]) {
+            for (int k = 0; k < N; k++) {
+              if (tap[k] == items[j]) {
+                if (nextUsingIdx[k] != 0) break;
+                nextUsingIdx[k] = j;
+                nextUsingCount++;
                 break;
               }
             }
           }
         }
-        using[products[i]] = true;
+
+        if (nextUsingCount != size) {
+          for (int j = 0; j < N; j++) {
+            if (nextUsingIdx[j] == 0) {
+              isUsing[tap[j]] = false;
+              tap[j] = item;
+              break;
+            }
+          }
+        }
+        else {
+          int maxIdx = 0;
+          for (int j = 1; j < N; j++) {
+            if (nextUsingIdx[j] > nextUsingIdx[maxIdx]) maxIdx = j;
+          }
+          isUsing[tap[maxIdx]] = false;
+          tap[maxIdx] = item;
+        }
+
+        count++;
       }
+      isUsing[item] = true;
     }
-    return count;
+    System.out.println(count);
   }
 }
--- baekjoon/greedy/multitap-scheduling/README.md
+++ baekjoon/greedy/multitap-scheduling/README.md
@@ -198,3 +198,78 @@ public class Main {
   }
 }
 ```
+
+**최종 코드(AC)**
+
+```java
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+
+    int N = Integer.parseInt(st.nextToken());
+    int K = Integer.parseInt(st.nextToken());
+
+    int[] tap = new int[N];
+    int size = 0;
+    boolean[] isUsing = new boolean[K + 1];
+
+    int[] items = new int[K];
+    st = new StringTokenizer(br.readLine());
+    for (int i = 0; i < K; i++) {
+      items[i] = Integer.parseInt(st.nextToken());
+    }
+
+    int count = 0;
+    for (int i = 0; i < K; i++) {
+      int item = items[i];
+      if (isUsing[item]) continue;
+
+      if (size < N) tap[size++] = item;
+      else {
+        int nextUsingCount = 0;
+        int[] nextUsingIdx = new int[N];
+        for (int j = i + 1; j < K; j++) {
+          if (isUsing[items[j]]) {
+            for (int k = 0; k < N; k++) {
+              if (tap[k] == items[j]) {
+                if (nextUsingIdx[k] != 0) break;
+                nextUsingIdx[k] = j;
+                nextUsingCount++;
+                break;
+              }
+            }
+          }
+        }
+
+        if (nextUsingCount != size) {
+          for (int j = 0; j < N; j++) {
+            if (nextUsingIdx[j] == 0) {
+              isUsing[tap[j]] = false;
+              tap[j] = item;
+              break;
+            }
+          }
+        }
+        else {
+          int maxIdx = 0;
+          for (int j = 1; j < N; j++) {
+            if (nextUsingIdx[j] > nextUsingIdx[maxIdx]) maxIdx = j;
+          }
+          isUsing[tap[maxIdx]] = false;
+          tap[maxIdx] = item;
+        }
+
+        count++;
+      }
+      isUsing[item] = true;
+    }
+    System.out.println(count);
+  }
+}
+```
