8b3b882533e4333708f318e41260f184893c02d6
jeongbeomSeo <jeongbeom4693@gmail.com>
2023-05-21 13:56:43 +0900

    Final Solve Problem

--- baekjoon/stack/postfix-notation/Main.java
+++ baekjoon/stack/postfix-notation/Main.java
@@ -7,31 +7,31 @@ public class Main {
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    String formula = br.readLine();
-
+    String str = br.readLine();
     Stack<Character> stack = new Stack<>();
     StringBuilder sb = new StringBuilder();
-    for (int i = 0; i < formula.length(); i++) {
-      char curChar = formula.charAt(i);
+
+    for (int i = 0; i < str.length(); i++) {
+      char curChar = str.charAt(i);
 
       if (curChar >= 'A' && curChar <= 'Z') sb.append(curChar);
-      else if (curChar == ')') {
-        while (stack.peek() != '(') {
-          sb.append(stack.pop());
+      else if (stack.isEmpty()) stack.add(curChar);
+      else {
+        if (curChar == '+' || curChar == '-') {
+          while (!stack.isEmpty() && stack.peek() != '(') sb.append(stack.pop());
+          stack.add(curChar);
         }
-        stack.pop();
+        else if (curChar == '*' || curChar == '/') {
+          if (stack.peek() == '*' || stack.peek() == '/') sb.append(stack.pop());
+          stack.add(curChar);
+        }
+        else if (curChar == '(') stack.add(curChar);
+        else {
+          while (stack.peek() != '(') sb.append(stack.pop());
+          stack.pop();
+        }
       }
-      else if (curChar == '+' || curChar == '-') {
-        while (!stack.isEmpty() && stack.peek() != '(') sb.append(stack.pop());
-        stack.add(curChar);
-      }
-      else if (curChar == '*' || curChar == '/') {
-        if (!stack.isEmpty() && (stack.peek() == '*' || stack.peek() == '/')) sb.append(stack.pop());
-        stack.add(curChar);
-      }
-      else stack.add(curChar);
     }
-
     while (!stack.isEmpty()) sb.append(stack.pop());
 
     System.out.println(sb);
