ba618ca703a4f0ae087ec6ea0be4c310b66df3f0
jeongbeomSeo <jeongbeom4693@gmail.com>
2023-05-23 16:23:38 +0900

    Better Solution

--- baekjoon/tier/platinum/planet-tunnel/Main.java
+++ baekjoon/tier/platinum/planet-tunnel/Main.java
@@ -12,47 +12,48 @@ public class Main {
     StringTokenizer st;
 
     int N = Integer.parseInt(br.readLine());
-    Point[] points = new Point[N];
+
+    Planet[] planets = new Planet[N];
     for (int i = 0; i < N; i++) {
       st = new StringTokenizer(br.readLine());
       int x = Integer.parseInt(st.nextToken());
       int y = Integer.parseInt(st.nextToken());
       int z = Integer.parseInt(st.nextToken());
-
-      points[i] = new Point(i, x, y, z);
+      planets[i] = new Planet(x, y, z, i);
     }
 
     ArrayList<Edge> edges = new ArrayList<>();
 
-    Arrays.sort(points, (p1, p2) -> p1.x - p2.x);
+    Arrays.sort(planets, (p1, p2) -> p1.x - p2.x);
     for (int i = 0; i < N - 1; i++) {
-      edges.add(new Edge(points[i].idx, points[i + 1].idx, Math.abs(points[i].x - points[i + 1].x)));
+      edges.add(new Edge(planets[i].idx, planets[i + 1].idx, Math.abs(planets[i].x - planets[i + 1].x)));
     }
 
-    Arrays.sort(points, (p1, p2) -> p1.y - p2.y);
+    Arrays.sort(planets, (p1, p2) -> p1.y - p2.y);
     for (int i = 0; i < N - 1; i++) {
-      edges.add(new Edge(points[i].idx, points[i + 1].idx, Math.abs(points[i].y - points[i + 1].y)));
+      edges.add(new Edge(planets[i].idx, planets[i + 1].idx, Math.abs(planets[i].y - planets[i + 1].y)));
     }
 
-    Arrays.sort(points, (p1, p2) -> p1.z - p2.z);
+    Arrays.sort(planets, (p1, p2) -> p1.z - p2.z);
     for (int i = 0; i < N - 1; i++) {
-      edges.add(new Edge(points[i].idx, points[i + 1].idx, Math.abs(points[i].z - points[i + 1].z)));
+      edges.add(new Edge(planets[i].idx, planets[i + 1].idx, Math.abs(planets[i].z - planets[i + 1].z)));
     }
 
-    Collections.sort(edges, (e1, e2) -> e1.cost - e2.cost);
+    Collections.sort(edges);
 
     int[] parent = new int[N];
     for (int i = 1; i < N; i++) parent[i] = i;
 
-    int sum = 0;
     int count = 0;
+    int sum = 0;
     for (int i = 0; i < edges.size(); i++) {
       if (union_merge(parent, edges.get(i).src, edges.get(i).dst)) {
-        sum += edges.get(i).cost;
         count++;
+        sum += edges.get(i).cost;
         if (count == N - 1) break;
       }
     }
+
     System.out.println(sum);
   }
   static int union_find(int[] parent, int x) {
@@ -65,31 +66,37 @@ public class Main {
     y = union_find(parent, y);
 
     if (x == y) return false;
-    parent[x] = y;
+    else parent[x] = y;
     return true;
   }
 }
-class Edge {
+class Edge implements Comparable<Edge>{
   int src;
   int dst;
   int cost;
 
-   Edge(int src, int dst, int cost) {
-     this.src = src;
-     this.dst = dst;
-     this.cost = cost;
-   }
+  Edge(int src, int dst, int cost) {
+    this.src = src;
+    this.dst = dst;
+    this.cost = cost;
+  }
+
+  @Override
+  public int compareTo(Edge o) {
+    return this.cost - o.cost;
+  }
 }
-class Point {
-  int idx;
+
+class Planet{
   int x;
   int y;
   int z;
+  int idx;
 
-  Point(int idx, int x, int y, int z) {
-    this.idx = idx;
+  Planet(int x, int y, int z, int idx) {
     this.x = x;
     this.y = y;
     this.z = z;
+    this.idx = idx;
   }
 }
--- baekjoon/tier/platinum/planet-tunnel/out/production/planet-tunnel/README.md
+++ baekjoon/tier/platinum/planet-tunnel/out/production/planet-tunnel/README.md
@@ -38,3 +38,105 @@
 ```
 4
 ```
+
+## 코드
+
+**AC**
+
+```java
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st;
+
+    int N = Integer.parseInt(br.readLine());
+    Point[] points = new Point[N];
+    for (int i = 0; i < N; i++) {
+      st = new StringTokenizer(br.readLine());
+      int x = Integer.parseInt(st.nextToken());
+      int y = Integer.parseInt(st.nextToken());
+      int z = Integer.parseInt(st.nextToken());
+
+      points[i] = new Point(i, x, y, z);
+    }
+
+    ArrayList<Edge> edges = new ArrayList<>();
+
+    Arrays.sort(points, (p1, p2) -> p1.x - p2.x);
+    for (int i = 0; i < N - 1; i++) {
+      edges.add(new Edge(points[i].idx, points[i + 1].idx, Math.abs(points[i].x - points[i + 1].x)));
+    }
+
+    Arrays.sort(points, (p1, p2) -> p1.y - p2.y);
+    for (int i = 0; i < N - 1; i++) {
+      edges.add(new Edge(points[i].idx, points[i + 1].idx, Math.abs(points[i].y - points[i + 1].y)));
+    }
+
+    Arrays.sort(points, (p1, p2) -> p1.z - p2.z);
+    for (int i = 0; i < N - 1; i++) {
+      edges.add(new Edge(points[i].idx, points[i + 1].idx, Math.abs(points[i].z - points[i + 1].z)));
+    }
+
+    Collections.sort(edges, (e1, e2) -> e1.cost - e2.cost);
+
+    int[] parent = new int[N];
+    for (int i = 1; i < N; i++) parent[i] = i;
+
+    int sum = 0;
+    int count = 0;
+    for (int i = 0; i < edges.size(); i++) {
+      if (union_merge(parent, edges.get(i).src, edges.get(i).dst)) {
+        sum += edges.get(i).cost;
+        count++;
+        if (count == N - 1) break;
+      }
+    }
+    System.out.println(sum);
+  }
+  static int union_find(int[] parent, int x) {
+    if (parent[x] == x) return x;
+
+    return parent[x] = union_find(parent, parent[x]);
+  }
+  static boolean union_merge(int[] parent, int x, int y) {
+    x = union_find(parent, x);
+    y = union_find(parent, y);
+
+    if (x == y) return false;
+    parent[x] = y;
+    return true;
+  }
+}
+class Edge {
+  int src;
+  int dst;
+  int cost;
+
+   Edge(int src, int dst, int cost) {
+     this.src = src;
+     this.dst = dst;
+     this.cost = cost;
+   }
+}
+class Point {
+  int idx;
+  int x;
+  int y;
+  int z;
+
+  Point(int idx, int x, int y, int z) {
+    this.idx = idx;
+    this.x = x;
+    this.y = y;
+    this.z = z;
+  }
+}
+```
