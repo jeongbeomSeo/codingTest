8a43b91b89a4e5442bec4ef4f3920b1cdcdf8e0c
jeongbeomSeo <jeongbeom4693@gmail.com>
2023-05-24 16:49:49 +0900

    Final Solve Problem

--- baekjoon/tier/gold/sum-of-subsequences-2/Main.java
+++ baekjoon/tier/gold/sum-of-subsequences-2/Main.java
@@ -1,7 +1,9 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
 
 public class Main {
   public static void main(String[] args) throws IOException {
@@ -11,65 +13,55 @@ public class Main {
     int N = Integer.parseInt(st.nextToken());
     int S = Integer.parseInt(st.nextToken());
 
-    int[] arr = new int[N];
+    int[] nums = new int[N];
     st = new StringTokenizer(br.readLine());
     for (int i = 0; i < N; i++) {
-      arr[i] = Integer.parseInt(st.nextToken());
+      nums[i] = Integer.parseInt(st.nextToken());
     }
 
     ArrayList<Integer> aSum = new ArrayList<>();
     ArrayList<Integer> bSum = new ArrayList<>();
 
-    combination(arr, aSum, 0, new int[N / 2 + 1], 0, N / 2);
-    combination(arr, bSum, N / 2, new int[N / 2 + 1], 0, N);
+    combination(nums, aSum, 0, N / 2, 0);
+    combination(nums, bSum, N / 2, N, 0);
 
     Collections.sort(bSum);
-    int size = bSum.size();
-
     long count = 0;
+    int size = bSum.size();
     for (int i = 0; i < aSum.size(); i++) {
       int target = S - aSum.get(i);
-
       count += upper_bound(bSum, 0, size, target) - lower_bound(bSum, 0, size, target);
     }
 
     if (S == 0) System.out.println(count - 1);
     else System.out.println(count);
+
   }
-  static void combination(int[] arr, ArrayList<Integer> sum, int ptr, int[] buffer, int size, int N) {
-    if (ptr == N) {
-      int result = 0;
-      for (int i = 0; i < size; i++) {
-        result += buffer[i];
-      }
-      sum.add(result);
+  static int lower_bound(ArrayList<Integer> sum, int left, int right, int target) {
+    while (left < right) {
+      int mid = (left + right) / 2;
+
+      if (sum.get(mid) < target) left = mid + 1;
+      else right = mid;
+    }
+    return left;
+  }
+  static int upper_bound(ArrayList<Integer> sum, int left, int right, int target) {
+    while (left < right) {
+      int mid = (left + right) / 2;
+
+      if(sum.get(mid) <= target) left = mid + 1;
+      else right = mid;
+    }
+    return left;
+  }
+  static void combination(int[] nums, ArrayList<Integer> sumList, int ptr, int size, int sum) {
+    if(ptr == size) {
+      sumList.add(sum);
     }
     else {
-      buffer[size] = arr[ptr];
-      combination(arr, sum, ptr + 1, buffer, size + 1, N);
-      buffer[size] = 0;
-
-      combination(arr, sum, ptr + 1 , buffer, size, N);
+      combination(nums, sumList, ptr + 1, size, sum + nums[ptr]);
+      combination(nums, sumList, ptr + 1, size, sum);
     }
-  }
-  static int upper_bound(ArrayList<Integer> bSum, int left, int right, int target) {
-    while (left < right) {
-      int mid = (left + right) / 2;
-      int value = bSum.get(mid);
-
-      if (value <= target) left = mid + 1;
-      else right = mid;
-    }
-    return left;
-  }
-  static int lower_bound(ArrayList<Integer> bSum, int left, int right, int target) {
-    while (left < right) {
-      int mid = (left + right) / 2;
-      int value = bSum.get(mid);
-
-      if (value < target) left = mid + 1;
-      else right = mid;
-    }
-    return left;
   }
 }
--- baekjoon/tier/gold/sum-of-subsequences-2/README.md
+++ baekjoon/tier/gold/sum-of-subsequences-2/README.md
@@ -33,6 +33,8 @@ N개의 정수로 이루어진 수열이 있을 때, 크기가 양수인 부분�
 
 ## 코드 
 
+**AC**
+
 ```java
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -107,6 +109,78 @@ public class Main {
       else right = mid;
     }
     return left;
+  }
+}
+```
+
+**최종 코드(AC)**
+
+```java
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+
+    int N = Integer.parseInt(st.nextToken());
+    int S = Integer.parseInt(st.nextToken());
+
+    int[] nums = new int[N];
+    st = new StringTokenizer(br.readLine());
+    for (int i = 0; i < N; i++) {
+      nums[i] = Integer.parseInt(st.nextToken());
+    }
+
+    ArrayList<Integer> aSum = new ArrayList<>();
+    ArrayList<Integer> bSum = new ArrayList<>();
+
+    combination(nums, aSum, 0, N / 2, 0);
+    combination(nums, bSum, N / 2, N, 0);
+
+    Collections.sort(bSum);
+    long count = 0;
+    int size = bSum.size();
+    for (int i = 0; i < aSum.size(); i++) {
+      int target = S - aSum.get(i);
+      count += upper_bound(bSum, 0, size, target) - lower_bound(bSum, 0, size, target);
+    }
+
+    if (S == 0) System.out.println(count - 1);
+    else System.out.println(count);
+
+  }
+  static int lower_bound(ArrayList<Integer> sum, int left, int right, int target) {
+    while (left < right) {
+      int mid = (left + right) / 2;
+
+      if (sum.get(mid) < target) left = mid + 1;
+      else right = mid;
+    }
+    return left;
+  }
+  static int upper_bound(ArrayList<Integer> sum, int left, int right, int target) {
+    while (left < right) {
+      int mid = (left + right) / 2;
+
+      if(sum.get(mid) <= target) left = mid + 1;
+      else right = mid;
+    }
+    return left;
+  }
+  static void combination(int[] nums, ArrayList<Integer> sumList, int ptr, int size, int sum) {
+    if(ptr == size) {
+      sumList.add(sum);
+    }
+    else {
+      combination(nums, sumList, ptr + 1, size, sum + nums[ptr]);
+      combination(nums, sumList, ptr + 1, size, sum);
+    }
   }
 }
 ```
