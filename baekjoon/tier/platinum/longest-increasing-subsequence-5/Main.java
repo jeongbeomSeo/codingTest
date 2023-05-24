47d2781d03590dcc4f360e1829caa0a8e05be890
jeongbeomSeo <jeongbeom4693@gmail.com>
2023-05-24 17:22:30 +0900

    Solve Problem Using BufferedWriter

--- baekjoon/tier/platinum/longest-increasing-subsequence-5/Main.java
+++ baekjoon/tier/platinum/longest-increasing-subsequence-5/Main.java
@@ -1,14 +1,14 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.StringTokenizer;
 
 public class Main {
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     StringTokenizer st;
 
     int N = Integer.parseInt(br.readLine());
+
     int[] nums = new int[N];
     st = new StringTokenizer(br.readLine());
     for (int i = 0; i < N; i++) {
@@ -17,16 +17,16 @@ public class Main {
 
     int[] buffer = new int[N];
     int size = 0;
-
-    buffer[size++] = nums[0];
     int[] idxList = new int[N];
+    buffer[0] = nums[0];
+    size++;
 
     for (int i = 1; i < N; i++) {
       if (buffer[size - 1] < nums[i]) {
+        buffer[size] = nums[i];
         idxList[i] = size;
-        buffer[size++] = nums[i];
+        size++;
       }
-      // else if (buffer[size - 1] > nums[i]) 이 방식으로도 하면 틀린다.
       else {
         int idx = lower_bound(buffer, 0, size, nums[i]);
         buffer[idx] = nums[i];
@@ -34,32 +34,17 @@ public class Main {
       }
     }
 
-    System.out.println(size);
     int[] result = new int[size];
-    int last = N - 1;
-
+    int ptr = N - 1;
     for (int i = size - 1; i >= 0; i--) {
-      for (int j = last; j >= 0; j--) {
-        if (idxList[j] == i) {
-          result[i] = nums[j];
-          last = j - 1;
-          break;
-        }
-      }
+      while (idxList[ptr] != i) ptr--;
+      result[i] = nums[ptr];
     }
-    /* 이 방식 쓰면 틀림
-    int last = 0;
-    for (int i = 0; i < size; i++) {
-      for (; last < N; last++) {
-        if (idxList[last] == i) {
-          result[i] = nums[last++];
-          break;
-        }
-      }
-    }
-    */
 
-   for (int i = 0; i < size; i++) System.out.print(result[i] + " ");
+    bw.write(size + "\n");
+    for (int ans : result) bw.write(ans + " ");
+    bw.flush();
+    bw.close();
 
   }
   static int lower_bound(int[] buffer, int left, int right, int target) {
