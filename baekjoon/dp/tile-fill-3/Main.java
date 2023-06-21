ef3c0967a70157a86ec1ae81fdfc9000f5df9ebd
jeongbeomSeo <jeongbeom4693@gmail.com>
2023-06-21 16:08:19 +0900

    Final Solve Problem

--- baekjoon/dp/tile-fill-3/Main.java
+++ baekjoon/dp/tile-fill-3/Main.java
@@ -1,24 +1,30 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
   static int MOD = 1000000007;
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-
-    int N = sc.nextInt();
+    int N = Integer.parseInt(br.readLine());
 
     long[][] dp = new long[1000001][2];
 
-    dp[0][0] = 0;
+    dp[0][0] = 1;
     dp[1][0] = 2;
-    dp[2][0] = 7;
-    dp[2][1] = 1;
+    dp[2][0] = 4 + 3;
+    dp[0][1] = 1;
+    dp[1][1] = 3;
+    dp[2][1] = 10;
 
     for (int i = 3; i <= N; i++) {
-      dp[i][1] = (dp[i - 3][0] + dp[i - 1][1]) % MOD;
+      long count = (dp[i - 1][0] * 2 + dp[i - 2][0] * 3) % MOD;
 
-      dp[i][0] = (2 * dp[i - 1][0] + 3 * dp[i - 2][0] + 2 * dp[i][1]) % MOD;
+      count = (count + 2 * dp[i - 3][1]) % MOD;
+
+      dp[i][0] = count;
+      dp[i][1] = (dp[i - 1][1] + dp[i][0]) % MOD;
     }
 
     System.out.println(dp[N][0]);
--- baekjoon/dp/tile-fill/README.md
+++ baekjoon/dp/tile-fill/README.md
@@ -35,3 +35,38 @@
 아래 그림은 3×12 벽을 타일로 채운 예시이다.
 
 ![](https://onlinejudgeimages.s3-ap-northeast-1.amazonaws.com/upload/images/2663_1.jpg)
+
+## 코드 
+
+```java
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int N = Integer.parseInt(br.readLine());
+
+    int[] dp = new int[31];
+
+    dp[0] = 1;
+    dp[1] = 0;
+    dp[2] = 3;
+    dp[3] = 0;
+
+    for (int i = 4; i <= N; i++) {
+      int result = 0;
+
+      result += 3 * dp[i - 2];
+
+      for (int j = i - 4; j >= 0; j -= 2) result += 2 * dp[j];
+
+      dp[i] = result;
+    }
+
+    System.out.println(dp[N]);
+  }
+}
+```
