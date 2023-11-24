46b1b13fa8f3361dbcd98dc543262024056747f0
jeongbeomSeo <jeongbeom4693@gmail.com>
2023-11-24 17:08:39 +0900

    복습중: 필요한 내용 재복습중

--- tier/gold/remote-control/Main.java
+++ tier/gold/remote-control/Main.java
@@ -1,144 +1,64 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Arrays;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-    static String N;
-    static int M;
+    private static int result = Integer.MIN_VALUE;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
-        N = br.readLine();
-        M = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
 
-        boolean[] isWorkingBtn = new boolean[10];
-        Arrays.fill(isWorkingBtn, true);
+        int M = Integer.parseInt(br.readLine());
+
+        boolean[] isBrokenBtn = new boolean[10];
         if (M > 0) {
             st = new StringTokenizer(br.readLine());
             for (int i = 0; i < M; i++) {
-                int btn = Integer.parseInt(st.nextToken());
-                isWorkingBtn[btn] = false;
+                isBrokenBtn[Integer.parseInt(st.nextToken())] = true;
             }
         }
 
-        System.out.println(queryResult(isWorkingBtn));
+        int MAX = getMAXNum(N);
+
+        System.out.println(queryResult(MAX, isBrokenBtn, N));
     }
-    private static int queryResult(boolean[] isWorkingBtn) {
-        Queue<Node> q = new ArrayDeque<>();
+    private static int queryResult(int MAX, boolean[] isBrokenBtn, int target) {
+        result = Math.abs(100 - target);
 
-        int size = N.length();
-
-        q.add(new Node("", 0, false, false));
-
-        int result = onlyChannelMoveCount();
-        while (!q.isEmpty()) {
-            Node curNode = q.poll();
-
-            if (curNode.ptr == size) {
-                result = Math.min(result, getCount(curNode));
-                continue;
-            }
-
-            if (!curNode.isConsiderValueSize) {
-                updateQueueByNearNumber(isWorkingBtn, q, curNode);
-            } else {
-                updateQueue(isWorkingBtn, q, curNode);
-            }
-        }
+        recursive(MAX, isBrokenBtn, target, "");
 
         return result;
     }
-    private static void updateQueue(boolean[] isWorkingBtn, Queue<Node> q, Node curNode) {
-        if (curNode.isSmall) {
-            int idx = getBigestNum(isWorkingBtn);
-            q.add(new Node(curNode.value + getChar(idx), curNode.ptr + 1, true, true));
-        } else {
-            int idx = getSmallestNum(isWorkingBtn);
-            q.add(new Node(curNode.value + getChar(idx), curNode.ptr + 1, true, false));
+    private static void recursive(int MAX, boolean[] isBrokenBtn, int target, String str) {
+
+        int curNum = 0;
+        if (!str.isEmpty()) {
+            curNum = Integer.parseInt(str);
+        }
+        int length = str.length();
+        if (curNum <= MAX && length <= String.valueOf(MAX).length()) {
+            if (!str.isEmpty()) {
+                result = Math.min(result, Math.abs(target - curNum) + length);
+            }
+
+            for (int i = 0; i <= 9; i++) {
+                if (!isBrokenBtn[i]) {
+                    recursive(MAX, isBrokenBtn, target, i + str);
+                }
+            }
         }
     }
-    private static int getBigestNum(boolean[] isWorkingBtn) {
-        for (int i = 9; i >= 0; i--) {
-            if (isWorkingBtn[i])
-                return i;
-        }
-        return -1;
-    }
-    private static int getSmallestNum(boolean[] isWorkingBtn) {
-        for (int i = 0; i <= 9; i++) {
-            if (isWorkingBtn[i])
-                return i;
-        }
-        return -1;
-    }
-    private static void updateQueueByNearNumber(boolean[] isWorkingBtn, Queue<Node> q, Node node) {
-        char c = N.charAt(node.ptr);
+    private static int getMAXNum(int N) {
 
-        int idx = getIdx(c);
+        String str = String.valueOf(N);
+        int length = str.length();
 
-        if (isWorkingBtn[idx]) {
-            q.add(new Node(node.value + c, node.ptr + 1, false, false));
-        }
+        String result = "2" +
+                "0".repeat(length);
 
-        int downNum = getNearestWorkingDownNumBtn(isWorkingBtn, idx);
-        if (idx != downNum)
-            q.add(new Node(node.value + getChar(downNum), node.ptr + 1, true, downNum < idx));
-
-        int upNum = getNearestWorkingUpNumBtn(isWorkingBtn, idx);
-        if (downNum != upNum)
-            q.add(new Node(node.value + getChar(upNum), node.ptr + 1, true, upNum < idx));
-
-    }
-    private static int getNearestWorkingUpNumBtn(boolean[] isWorkingBtn, int idx) {
-
-        while (!isWorkingBtn[idx]) {
-            idx = upIdx(idx);
-        }
-
-        return idx;
-    }
-    private static int getNearestWorkingDownNumBtn(boolean[] isWorkingBtn, int idx) {
-
-        while (!isWorkingBtn[idx]) {
-            idx = downIdx(idx);
-        }
-
-        return idx;
-    }
-    private static char getChar(int idx) {
-        return (char)(idx + '0');
-    }
-    private static int upIdx(int idx) {
-        return idx + 1 != 10 ? idx + 1 : 0;
-    }
-    private static int downIdx(int idx) {
-        return idx - 1 != -1 ? idx - 1 : 9;
-    }
-    private static int getIdx(char c) {
-        return c - '0';
-    }
-    private static int getCount(Node node) {
-        return Math.abs(Integer.parseInt(node.value) - Integer.parseInt(N)) + N.length();
-    }
-    private static int onlyChannelMoveCount() {
-        return Math.abs(100 - Integer.parseInt(N));
-    }
-    static class Node {
-        String value;
-        int ptr;
-        boolean isConsiderValueSize;
-        boolean isSmall;
-
-        Node(String value, int ptr, boolean isConsiderValueSize, boolean isSmall) {
-            this.value = value;
-            this.ptr = ptr;
-            this.isConsiderValueSize = isConsiderValueSize;
-            this.isSmall = isSmall;
-        }
+        return Integer.parseInt(result);
     }
 }
