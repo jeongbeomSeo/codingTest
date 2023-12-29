9fa0deff3dd9e7accd5182f8a702e74ecc3f6f5d
jeongbeomSeo <jeongbeom4693@gmail.com>
2023-12-29 13:43:58 +0900

    복습중: 버블 소트 1838 복습 완료

--- algorithm/sort/bubble-sort-1838/Main.java
+++ algorithm/sort/bubble-sort-1838/Main.java
@@ -4,42 +4,48 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st;
+
+    int N = Integer.parseInt(br.readLine());
+    Number[] numberArray = new Number[N];
+
+    st = new StringTokenizer(br.readLine());
+    for (int i = 0; i < N; i++) {
+      int value = Integer.parseInt(st.nextToken());
+
+      numberArray[i] = new Number(i, value);
+    }
+
+    Arrays.sort(numberArray);
+
+    System.out.println(getQueryResult(numberArray, N));
+  }
+  private static int getQueryResult(Number[] numberArray, int N) {
+
+    int max = Integer.MIN_VALUE;
+
+    for (int i = 0; i < N; i++) {
+      max = Math.max(max, numberArray[i].originIdx - i);
+    }
+
+    if (max == Integer.MIN_VALUE) return 0;
+    return max;
+  }
+}
 class Number implements Comparable<Number>{
-  int idx;
+  int originIdx;
   int value;
 
-  Number(int idx, int value) {
-    this.idx = idx;
+  Number (int originIdx, int value) {
+    this.originIdx = originIdx;
     this.value = value;
   }
 
   @Override
   public int compareTo(Number o) {
     return this.value - o.value;
-  }
-}
-public class Main {
-
-  public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    StringTokenizer st;
-
-    int N = Integer.parseInt(br.readLine());
-
-    Number[] nums = new Number[N];
-    st = new StringTokenizer(br.readLine());
-    for(int i = 0; i < N; i++) {
-      nums[i] = new Number(i, Integer.parseInt(st.nextToken()));
-    }
-
-    Arrays.sort(nums);
-
-    int max = Integer.MIN_VALUE;
-    for(int i = 0; i < N; i++) {
-      if(nums[i].idx - i > max) max = nums[i].idx - i;
-    }
-
-    System.out.println(max);
-
   }
 }
--- algorithm/sort/bubble-sort-1838/out/production/bubble-sort-1838/README.md
+++ algorithm/sort/bubble-sort-1838/out/production/bubble-sort-1838/README.md
@@ -145,9 +145,10 @@ public class Main {
 
 **AC**
 
-|메모리|시간|
-|---|---|
-|84864KB|924ms|
+| CASE              | 메모리     | 시간     |
+|-------------------|---------|--------|
+| Merge Sort        | 84864KB | 924ms  |
+| Built-In Function | 72380KB | 1616ms |
 
 ```java
 import java.io.BufferedReader;
@@ -244,3 +245,52 @@ public class Main {
 }
 ```
 
+**Bulit-In Function 사용시**
+
+```java
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+class Number implements Comparable<Number>{
+  int idx;
+  int value;
+
+  Number(int idx, int value) {
+    this.idx = idx;
+    this.value = value;
+  }
+
+  @Override
+  public int compareTo(Number o) {
+    return this.value - o.value;
+  }
+}
+public class Main {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st;
+
+    int N = Integer.parseInt(br.readLine());
+
+    Number[] nums = new Number[N];
+    st = new StringTokenizer(br.readLine());
+    for(int i = 0; i < N; i++) {
+      nums[i] = new Number(i, Integer.parseInt(st.nextToken()));
+    }
+
+    Arrays.sort(nums);
+
+    int max = Integer.MIN_VALUE;
+    for(int i = 0; i < N; i++) {
+      if(nums[i].idx - i > max) max = nums[i].idx - i;
+    }
+
+    System.out.println(max);
+
+  }
+}
+```
