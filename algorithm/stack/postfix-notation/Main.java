a722170e5a03df291a0482312909af5dec45c366
jeongbeomSeo <jeongbeom4693@gmail.com>
2024-01-05 22:26:16 +0900

    복습중: 후위 표기식

--- algorithm/stack/postfix-notation/Main.java
+++ algorithm/stack/postfix-notation/Main.java
@@ -1,39 +1,52 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Stack;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 public class Main {
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
     String str = br.readLine();
-    Stack<Character> stack = new Stack<>();
+
+    System.out.println(getPostPrefix(str));
+  }
+  private static String getPostPrefix(String str) {
+
     StringBuilder sb = new StringBuilder();
-
+    Deque<Character> stack = new ArrayDeque<>();
     for (int i = 0; i < str.length(); i++) {
-      char curChar = str.charAt(i);
+      char c = str.charAt(i);
 
-      if (curChar >= 'A' && curChar <= 'Z') sb.append(curChar);
-      else if (stack.isEmpty()) stack.add(curChar);
-      else {
-        if (curChar == '+' || curChar == '-') {
-          while (!stack.isEmpty() && stack.peek() != '(') sb.append(stack.pop());
-          stack.add(curChar);
+      if (isOpr(c)) {
+        if (c == '*' || c == '/') {
+          while (!stack.isEmpty() && (stack.peekFirst() == '*' || stack.peekFirst() == '/')) {
+            sb.append(stack.pollFirst());
+          }
+          stack.addFirst(c);
+        } else {
+          while (!stack.isEmpty() && stack.peekFirst() != '(') {
+            sb.append(stack.pollFirst());
+          }
+          stack.addFirst(c);
         }
-        else if (curChar == '*' || curChar == '/') {
-          if (stack.peek() == '*' || stack.peek() == '/') sb.append(stack.pop());
-          stack.add(curChar);
+      } else if (c == '(') {
+        stack.addFirst(c);
+      } else if (c == ')') {
+        while (!stack.isEmpty() && stack.peekFirst() != '(') {
+          sb.append(stack.pollFirst());
         }
-        else if (curChar == '(') stack.add(curChar);
-        else {
-          while (stack.peek() != '(') sb.append(stack.pop());
-          stack.pop();
-        }
+        stack.pollFirst();
+      } else {
+        sb.append(c);
       }
     }
-    while (!stack.isEmpty()) sb.append(stack.pop());
+    while (!stack.isEmpty()) sb.append(stack.pollFirst());
 
-    System.out.println(sb);
+    return sb.toString();
+  }
+  private static boolean isOpr(char c) {
+    return c == '+' || c == '-' || c == '*' || c == '/';
   }
 }
