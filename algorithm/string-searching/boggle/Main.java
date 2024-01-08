2997bcaac4cc4bd89507868cfb22af5a33e8eb88
jeongbeomSeo <jeongbeom4693@gmail.com>
2024-01-08 19:45:26 +0900

    복습중: Boggle 트라이로 복습을 했지만 이전에 풀이했었던 방식이 약 4배 가까이 빠른 속도의 성능을 보여줌 이전 풀이의 경우, 주어진 dictionary의 문자열을 하나씩 확인하면서 주어진 board에 만족하는 경우가 있는지 bfs로 탐색하는 방식으로 해당 방식의 풀이가 불필요한 로직 없이 깔끔해서 더 빠른 속도를 보여줌

--- algorithm/string-searching/boggle/Main.java
+++ algorithm/string-searching/boggle/Main.java
@@ -10,9 +10,9 @@ public class Main {
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    int w = Integer.parseInt(br.readLine());
-
     final Tree tree = new Tree();
+
+    int w = Integer.parseInt(br.readLine());
     for (int i = 0; i < w; i++) {
       String str = br.readLine();
 
@@ -21,8 +21,9 @@ public class Main {
 
     // 띄어쓰기 제거
     br.readLine();
+
     int b = Integer.parseInt(br.readLine());
-    while (b-- != 0) {
+    while(b-- != 0) {
 
       char[][] board = new char[4][4];
       for (int i = 0; i < 4; i++) {
@@ -33,137 +34,137 @@ public class Main {
         }
       }
 
-      String result = getResult(tree, board);
+      String output = query(tree, board);
 
-      System.out.println(result);
+      System.out.println(output);
 
       // 띄어쓰기 제거
       if (b != 0) {
         br.readLine();
       }
     }
-
   }
-  private static String getResult(Tree tree, char[][] board) {
+  private static String query(Tree tree, char[][] board) {
 
-    List<Result> resultList = new ArrayList<>();
-
-    // Size: 1부터 시작
-    resultList.add(new Result("", new boolean[4][4],0));
+    Set<String> result = new TreeSet<>(new CustomStringComparator());
 
     for (int row = 0; row < 4; row++) {
       for (int col = 0; col < 4; col++) {
+        if (tree.root.childNode.containsKey(board[row][col])) {
+          Set<String> dfsResult = dfs(tree.root.childNode.get(board[row][col]), board, row, col);
 
-        Queue<Result> buffer = new ArrayDeque<>();
-        for (int i = 0; i < resultList.size(); i++) {
-          Result curResult = resultList.get(i);
-
-          if (!curResult.isVisited[row][col] && tree.root.childNode.containsKey(board[row][col])) {
-            // dfs할 때 어떤 Node를 건네주냐에 따라서 root Node를 건네줄지, root의 child Node를 건네주는지 바뀜.
-            List<Status> dfsResult = dfs(tree.root.childNode.get(board[row][col]), board, row, col, curResult.isVisited);
-
-            for (int k = 0; k < dfsResult.size(); k++) {
-              Status curDfsResult = dfsResult.get(k);
-              // 반복문 돌리고 있는 target List에 요소를 추가할 경우 생각치 못한 로직으로 돌아갈 가능성이 높음 매우 주의
-              buffer.add(new Result(
-                      curResult.findedStrList, curDfsResult.curStr, curDfsResult.isVisited, curResult.score + getScore(curDfsResult.curStr)));
-            }
-          }
+          result.addAll(dfsResult);
         }
-        while (!buffer.isEmpty()) resultList.add(buffer.poll());
       }
     }
 
-    Collections.sort(resultList);
-    Result finalResult = resultList.get(0);
-    for (int i = 0; i < 4; i++) {
-      for (int j = 0; j < 4; j++) {
-        System.out.print(finalResult.isVisited[i][j] ? "1" : "0");
-      }
-      System.out.println();
-    }
     StringBuilder sb = new StringBuilder();
-    sb.append(finalResult.score + " ");
-
-    Collections.sort(finalResult.findedStrList, new CustomStringCompartor());
-    sb.append(finalResult.findedStrList.get(0) + " ");
-    sb.append(finalResult.findedStrList.size() - 1);
+    sb.append(getMaxScore(result) + " ");
+    sb.append(result.toArray()[0] + " ");
+    sb.append(result.size());
 
     return sb.toString();
   }
-  private static int getScore(String value) {
-    int len = value.length();
+  private static int getMaxScore(Set<String> result) {
+    int score = 0;
 
-    if (len == 1 || len == 2) return 0;
-    else if (len == 3 || len == 4) return 1;
-    else if (len == 5) return 2;
-    else if (len == 6) return 3;
-    else if (len == 7) return 5;
-    else if (len == 8) return 11;
+    for (String str : result) {
+      score += getScore(str.length());
+    }
 
-    System.out.println("getScore 로직 오류");
-    return 0;
+    return score;
   }
-  private static List<Status> dfs(Node initNode, char[][] board, int initRow, int initCol, boolean[][] initIsVisited) {
+  private static int getScore(int strLen) {
+    if (strLen == 1 || strLen == 2) return 0;
+    else if (strLen == 3 || strLen == 4) return 1;
+    else if (strLen == 5) return 2;
+    else if (strLen == 6) return 3;
+    else if (strLen == 7) return 5;
+
+    return 11;
+  }
+  private static Set<String> dfs(Node initNode, char[][] board, int initRow, int initCol) {
 
     Queue<Status> q = new ArrayDeque<>();
-    Point initPoint = new Point(initRow, initCol);
-    boolean[][] newIsVisited = copyBooleanGrid(initIsVisited);
-    newIsVisited[initRow][initCol] = true;
-    q.add(new Status(initPoint, "" + board[initRow][initCol], newIsVisited, initNode));
+    boolean[][] isVisited = new boolean[4][4];
+    isVisited[initRow][initCol] = true;
+    q.add(new Status(initRow, initCol, isVisited, board[initRow][initCol], initNode));
 
-    List<Status> result = new ArrayList<>();
+    Set<String> result = new HashSet<>();
     while (!q.isEmpty()) {
       Status curStatus = q.poll();
 
-      if (curStatus.curNode.isEnd) {
-        result.add(curStatus);
+      if (curStatus.node.isEnd) {
+        result.add(curStatus.str);
       }
 
       for (int i = 0; i < 8; i++) {
-        int nxtRow = curStatus.point.row + DR[i];
-        int nxtCol = curStatus.point.col + DC[i];
+        int nxtRow = curStatus.row + DR[i];
+        int nxtCol = curStatus.col + DC[i];
 
         if (isValidPoint(nxtRow, nxtCol)
                 && !curStatus.isVisited[nxtRow][nxtCol]
-                && curStatus.curNode.childNode.containsKey(board[nxtRow][nxtCol])) {
-          char nxtKey = board[nxtRow][nxtCol];
+                && curStatus.node.childNode.containsKey(board[nxtRow][nxtCol])) {
+
+          char curChar = board[nxtRow][nxtCol];
           boolean[][] nxtIsVisited = copyBooleanGrid(curStatus.isVisited);
           nxtIsVisited[nxtRow][nxtCol] = true;
-          q.add(new Status(new Point(nxtRow, nxtCol), curStatus.curStr + nxtKey,
-                  nxtIsVisited, curStatus.curNode.childNode.get(nxtKey)));
+          q.add(new Status(nxtRow, nxtCol, nxtIsVisited,
+                  curStatus.str + curChar, curStatus.node.childNode.get(curChar)));
         }
       }
     }
 
     return result;
   }
-  private static boolean isValidPoint(int row, int col) {
-    return row >= 0 && col >= 0 && row < 4 && col < 4;
-  }
-  private static boolean[][] copyBooleanGrid(boolean[][] origin) {
-    int rowLen = origin.length;
-    int colLen = origin[0].length;
+  private static boolean[][] copyBooleanGrid(boolean[][] grid) {
+    int rowLen = grid.length;
+    int colLen = grid[0].length;
     boolean[][] copy = new boolean[rowLen][colLen];
 
     for (int i = 0; i < rowLen; i++) {
       for (int j = 0; j < colLen; j++) {
-        copy[i][j] = origin[i][j];
+        copy[i][j] = grid[i][j];
       }
     }
 
     return copy;
   }
+  private static boolean isValidPoint(int row, int col) {
+    return row >= 0 && col >= 0 && row < 4 && col < 4;
+  }
 }
-class CustomStringCompartor implements Comparator<String> {
+class Status {
+  int row;
+  int col;
+  boolean[][] isVisited;
+  String str;
+  Node node;
+
+  Status(int row, int col, boolean[][] isVisited, char initChar, Node node) {
+    this.row = row;
+    this.col = col;
+    this.isVisited = isVisited;
+    str = "" + initChar;
+    this.node = node;
+  }
+
+  Status(int row, int col, boolean[][] isVisited, String str, Node node) {
+    this.row = row;
+    this.col = col;
+    this.isVisited = isVisited;
+    this.str = str;
+    this.node = node;
+  }
+
+
+}
+class CustomStringComparator implements Comparator<String> {
   @Override
   public int compare(String o1, String o2) {
-    int len1 = o1.length();
-    int len2 = o2.length();
-
-    if (len1 != len2) return len2 - len1;
+    if (o1.length() != o2.length()) return o2.length() - o1.length();
     else {
-      for (int i = 0; i < len1; i++) {
+      for (int i = 0; i < o1.length(); i++) {
         char c1 = o1.charAt(i);
         char c2 = o2.charAt(i);
 
@@ -173,86 +174,8 @@ class CustomStringCompartor implements Comparator<String> {
     return 0;
   }
 }
-class Result implements Comparable<Result>{
-  List<String> findedStrList;
-  boolean[][] isVisited;
-  int score;
-
-  Result(String value, boolean[][] isVisited, int score) {
-    this.findedStrList = new ArrayList<>();
-    this.findedStrList.add(value);
-    this.isVisited = isVisited;
-    this.score = score;
-  }
-
-  Result(List<String> prevFindedStrList, String value, boolean[][] isVisited, int score) {
-    this.findedStrList = new ArrayList<>(prevFindedStrList);
-    findedStrList.add(value);
-    this.isVisited = isVisited;
-    this.score = score;
-  }
-
-  @Override
-  public int compareTo(Result o) {
-    // 점수가 같은 결과가 여러 개인 경우는 주어져 있지 않음.
-    return o.score - this.score;
-  }
-}
-class Status {
-  Point point;
-  String curStr;
-  boolean[][] isVisited;
-  Node curNode;
-
-  // 외부에서 전부 initializing 해서 건네주기
-  Status(Point point, String curStr, boolean[][] isVisted, Node curNode) {
-    this.point = point;
-    this.curStr = curStr;
-    this.isVisited = isVisted;
-    this.curNode = curNode;
-  }
-
-/*  private List<Point> initVisitedHistory(Point point) {
-    List<Point> visitHistory = new ArrayList<>();
-
-    visitHistory.add(point);
-    return visitHistory;
-  }*/
-
-/*  private boolean[][] initIsVisited(Point point) {
-    boolean[][] isVisited = new boolean[4][4];
-
-    isVisited[point.row][point.col] = true;
-    return isVisited;
-  }*/
-
-}
-class Point {
-  int row;
-  int col;
-
-  Point(int row, int col) {
-    this.row = row;
-    this.col = col;
-  }
-
-  @Override
-  public int hashCode() {
-    return row * 39 + col;
-  }
-
-  @Override
-  public boolean equals(Object obj) {
-    if (obj instanceof Point) {
-      Point p = (Point) obj;
-
-      return this.row == p.row && this.col == p.col;
-    }
-    return false;
-  }
-}
 class Tree {
-  final Node root = new Node(' ');
+  final Node root = new Node();
 
   public void update(String str) {
     Node curNode = root;
@@ -262,18 +185,23 @@ class Tree {
 
       curNode = curNode.childNode.computeIfAbsent(curChar, key -> new Node(curChar));
     }
-
     curNode.isEnd = true;
   }
+
 }
 class Node {
   Map<Character, Node> childNode;
   char value;
   boolean isEnd;
 
+  Node() {
+    childNode = new HashMap<>();
+    value = ' ';
+    isEnd = false;
+  }
   Node(char value) {
-    this.value = value;
-    this.childNode = new HashMap<>();
+    childNode = new HashMap<>();
+    value = value;
     isEnd = false;
   }
 }
