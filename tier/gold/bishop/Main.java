9a2ad74e91ca4a6f0c1d7d64156728af37dbf94d
jeongbeomSeo <jeongbeom4693@gmail.com>
2024-01-14 17:11:22 +0900

    복습중: Bishop 재풀이

--- tier/gold/bishop/Main.java
+++ tier/gold/bishop/Main.java
@@ -4,7 +4,8 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    private static int MAX = Integer.MIN_VALUE;
+    private static int oddCount = 0;
+    private static int evenCount = 0;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
@@ -19,55 +20,62 @@ public class Main {
             }
         }
 
-        System.out.println(queryResult(grid, N));
+        System.out.println(query(grid, N));
     }
-    private static int queryResult(int[][] grid, int N) {
+    public static int query(int[][] grid, int N) {
+        oddCount = 0;
+        evenCount = 0;
+
         boolean[] flag = new boolean[2 * N - 1];
+        //Even Calc
+        dfs(grid, flag, 0, 0, 0, N);
+        // Odd Calc
+        dfs(grid, flag, 1, 0, 0, N);
 
-        MAX = Integer.MIN_VALUE;
-        dfs(grid, flag, 0, N, 0);
-        int eventResult = MAX;
-
-        MAX = Integer.MIN_VALUE;
-        dfs(grid, flag, 1, N, 0);
-        int oddResult = MAX;
-
-        return eventResult + oddResult;
+        return evenCount + oddCount;
     }
-    private static void dfs(int[][] grid, boolean[] flag, int ptr, int size, int count) {
-        if (ptr >= 2 * size - 1) {
-            MAX = Math.max(MAX, count);
-        } else {
-            int row = ptr <= (size - 1) ? ptr : (size - 1);
-            int col = ptr <= (size - 1) ? 0 : ptr - (size - 1);
+    public static void dfs(int[][] grid, boolean[] flag, int startRow, int startCol, int count, int N) {
 
-            if (canLoadBishop(grid, flag, row, col, size)) {
-                while (isValidPoint(row, col, size)) {
-                    if (grid[row][col] == 1 && !flag[(size - 1) - row + col]) {
-                        flag[(size - 1) - row + col] = true;
-                        dfs(grid, flag, ptr + 2, size, count + 1);
-                        flag[(size - 1) - row + col] = false;
-                    }
-                    row--;
-                    col++;
-                }
+        if (startRow + startCol >= 2 * N - 1) {
+            if ((startRow + startCol) % 2 == 0) {
+                evenCount = Math.max(evenCount, count);
             } else {
-                dfs(grid, flag, ptr + 2, size, count);
+                oddCount = Math.max(oddCount, count);
             }
-        }
-    }
-    private static boolean canLoadBishop(int[][] grid, boolean[] flag, int row, int col, int size) {
-
-        while (isValidPoint(row, col, size)) {
-            if (grid[row][col] == 1 && !flag[(size - 1) - row + col]) return true;
-
-            row--;
-            col++;
+            return;
         }
 
-        return false;
+        int curRow = startRow;
+        int curCol = startCol;
+
+        boolean checkOk = false;
+        int[] nxtPoint = getNxtPoint(startRow, startCol, N);
+        while (isValidPoint(curRow, curCol, N)) {
+            if (!flag[(N - 1) - curRow + curCol] && grid[curRow][curCol] == 1) {
+                flag[(N - 1) - curRow + curCol] = true;
+                dfs(grid, flag, nxtPoint[0], nxtPoint[1], count + 1, N);
+                flag[(N - 1) - curRow + curCol] = false;
+
+                checkOk = true;
+            }
+            curRow--;
+            curCol++;
+        }
+        if (!checkOk) dfs(grid, flag, nxtPoint[0], nxtPoint[1], count, N);
     }
-    private static boolean isValidPoint(int row, int col, int size) {
-        return row >= 0 && col >= 0 && row < size && col < size;
+    private static int[] getNxtPoint(int row, int col, int N) {
+        if (row + 2 <= N - 1) {
+            return new int[]{row + 2, 0};
+        } else if (row + 2 == N) {
+            return new int[]{N - 1, 1};
+        } else if (row == N - 1) {
+            return new int[]{N - 1, col + 2};
+        }
+
+        System.out.println("Logic Error");
+        return new int[]{-1, -1};
+    }
+    private static boolean isValidPoint(int row, int col, int N) {
+        return row >= 0 && col >= 0 && row < N && col < N;
     }
 }
