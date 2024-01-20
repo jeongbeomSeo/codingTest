35b81c63710af93581a466cf8ca9e97276736219
jeongbeomSeo <jeongbeom4693@gmail.com>
2024-01-20 17:58:29 +0900

    캐슬 디펜스: 재풀이 성능 개선을 위해서 아래 2가지를 풀이에 추가 1. 현재 궁수 배치가 이론상 나올 수 있는 가장 베스트 배치라면 더이상 탐색 중지 2. 시뮬레이션 함수 로직에서 다음 스텝으로 넘어가기 위한 Grid 생성 과정을 없애고 rowLen을 1만큼 줄여주는 것으로 로직 최적화 하지만, 결국 이전 풀이가 성능이 더 좋았는데 그 이유는 BFS 특성상 distance가 작은 것부터 차곡차곡 꺼내지기 때문에 왼쪽, 위, 오른쪽 탐색으로 진행하던 도중 grid의 요소가 1인 Point를 발견했을 때 로직을 종료하는 것이 가장 베스트 접근 풀이임

--- tier/gold/castle-defence/Main.java
+++ tier/gold/castle-defence/Main.java
@@ -4,9 +4,10 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-  private static int[] DR = {0, -1, 0};
-  private static int[] DC = {-1, 0, 1};
-  private static int max = Integer.MIN_VALUE;
+  private static int ENEMY_COUNT = 0;
+  private static int MAX;
+  private static final int[] DR = {-1, 0, 1, 0};
+  private static final int[] DC = {0, 1, 0, -1};
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     StringTokenizer st = new StringTokenizer(br.readLine());
@@ -16,135 +17,180 @@ public class Main {
     int D = Integer.parseInt(st.nextToken());
 
     int[][] grid = new int[N][M];
+
     for (int i = 0; i < N; i++) {
       st = new StringTokenizer(br.readLine());
       for (int j = 0; j < M; j++) {
         grid[i][j] = Integer.parseInt(st.nextToken());
+
+        if (grid[i][j] == 1) ENEMY_COUNT++;
       }
     }
 
-    System.out.println(queryWithDFS(grid, N, M, D));
+    System.out.println(simulationWithDFS(grid, N, M, D));
   }
-  private static int queryWithDFS(int[][] grid, int N, int M, int D) {
-    max = Integer.MIN_VALUE;
+  private static int simulationWithDFS(int[][] grid, int N, int M, int D) {
 
-    dfs(grid, new int[3], 0, N, M, 0, D);
+    MAX = Integer.MIN_VALUE;
+    dfs(grid, new int[3], 0, M, 0, N, D);
 
-    return max;
+    return MAX;
   }
-  private static void dfs(int[][] grid, int[] buffer, int ptr, int N, int M, int size, int D) {
+  private static void dfs(int[][] grid, int[] buffer, int ptr, int colLen, int size, int rowLen, int arrowRange) {
+
+    // 성능 개선 1 => 648ms -> 396ms
+    if (MAX == ENEMY_COUNT || MAX == (3 * rowLen)) return;
 
     if (size == 3) {
-      int[][] copyGrid = copyGrid(grid, N, M);
-      max = Math.max(max, getMaxKill(copyGrid, buffer, N, M, D));
-    }
-    else if (ptr != M) {
+      int[][] copy = copyIntGrid(grid, rowLen, colLen);
+      MAX = Math.max(MAX, simulation(copy, buffer, rowLen, colLen, arrowRange));
+    } else if (ptr < colLen) {
       buffer[size] = ptr;
-      dfs(grid, buffer, ptr + 1, N, M, size + 1, D);
+      dfs(grid, buffer, ptr + 1, colLen, size + 1, rowLen, arrowRange);
       buffer[size] = 0;
 
-      dfs(grid, buffer, ptr + 1, N, M, size, D);
+      dfs(grid, buffer, ptr + 1, colLen, size, rowLen, arrowRange);
     }
   }
-  private static int[][] copyGrid(int[][] grid, int N, int M) {
-    int[][] copy = new int[N][M];
+  private static int simulation(int[][] grid, int[] buffer, int rowLen, int colLen, int arrowRange) {
 
-    for (int i = 0; i < N; i++) {
-      for (int j = 0; j < M; j++) {
-        copy[i][j] = grid[i][j];
+    int result = 0;
+    while (isContinue(grid, rowLen, colLen)) {
+      Queue<Point> delEnemyPointBuffer = new ArrayDeque<>();
+      for (int i = 0; i < 3; i++) {
+        int arrowColPos = buffer[i];
+
+        Point delEnemyPoint = findEnemy(grid, rowLen, arrowColPos, rowLen, colLen, arrowRange);
+        if (delEnemyPoint != null) delEnemyPointBuffer.add(delEnemyPoint);
       }
-    }
 
-    return copy;
+      result += killEnemyCount(grid, delEnemyPointBuffer);
+
+      // 성능 개선 2 => 396ms -> 332ms
+      rowLen--;
+      //grid = getNextStepGrid(grid, rowLen, colLen);
+    }
+    return result;
   }
-  private static int getMaxKill(int[][] grid, int[] buffer, int N, int M, int D) {
+  private static int killEnemyCount(int[][] grid, Queue<Point> delEnemyPointBuffer) {
 
     int killCount = 0;
-    while (isAliveEnemy(grid, N, M)) {
-      for (int i = 2; i >= 0; i--) {
-        int arrowPos = buffer[i];
+    while (!delEnemyPointBuffer.isEmpty()) {
+      Point delEnemyPoint = delEnemyPointBuffer.poll();
 
-        Status resultStatus = getKilledEnemyPos(grid, N, arrowPos, M, D);
-        if (resultStatus != null) {
-          killCount++;
-          grid[resultStatus.row][resultStatus.col] = 0;
-        }
+      if (grid[delEnemyPoint.row][delEnemyPoint.col] == 1) {
+        killCount++;
       }
-      grid = getGridWhenNextStep(grid, N, M);
+
+      grid[delEnemyPoint.row][delEnemyPoint.col] = 0;
     }
 
     return killCount;
   }
-  private static int[][] getGridWhenNextStep(int[][] grid, int N, int M) {
-    int[][] copy = new int[N][M];
+  private static Point findEnemy(int[][] grid, int arrowRowPos, int arrowColPos, int rowLen, int colLen, int arrowRange) {
 
-    for (int i = 0; i < N - 1; i++) {
-      for (int j = 0; j < M; j++) {
-        copy[i + 1][j] = grid[i][j];
+    boolean[][] isVisited = new boolean[rowLen][colLen];
+    Queue<Status> buffer = new ArrayDeque<>();
+    buffer.add(new Status(arrowRowPos - 1, arrowColPos, 1));
+    isVisited[arrowRowPos - 1][arrowColPos] = true;
+
+    Queue<RegistryInfo> pq = new PriorityQueue<>(new RegistryInfoCompartor());
+    while (!buffer.isEmpty()) {
+      Status curStatus = buffer.poll();
+
+      if (grid[curStatus.point.row][curStatus.point.col] == 1) {
+        pq.add(new RegistryInfo(curStatus. point, calcDistance(arrowRowPos, arrowColPos, curStatus.point.row, curStatus.point.col)));
       }
-    }
+      if (curStatus.count == arrowRange) continue;
 
-    return copy;
-  }
-  private static Status getKilledEnemyPos(int[][] grid, int N, int arrowPos, int M, int D) {
+      for (int i = 0; i < 4; i++) {
+        int nxtRow = curStatus.point.row + DR[i];
+        int nxtCol = curStatus.point.col + DC[i];
 
-    boolean[][] isVisited = new boolean[N][M];
-    Queue<Status> queueStatus = new ArrayDeque<>();
-    queueStatus.add(new Status(N - 1, arrowPos, 1));
-    isVisited[N - 1][arrowPos] = true;
-
-    Queue<Status> resultByPq = new PriorityQueue<>(new ComparatorStatus());
-    while (!queueStatus.isEmpty()) {
-      Status curStatus = queueStatus.poll();
-
-      if (grid[curStatus.row][curStatus.col] == 1) {
-        resultByPq.add(curStatus);
-      }
-      if (curStatus.count == D) continue;
-
-      for (int i = 0; i < 3; i++) {
-        int nxtRow = curStatus.row + DR[i];
-        int nxtCol = curStatus.col + DC[i];
-
-        if (isValidPoint(nxtRow, nxtCol, N, M) && !isVisited[nxtRow][nxtCol]) {
-          queueStatus.add(new Status(nxtRow, nxtCol, curStatus.count + 1));
+        if (isValidPoint(nxtRow, nxtCol, rowLen, colLen) && !isVisited[nxtRow][nxtCol]) {
+          buffer.add(new Status(nxtRow, nxtCol, curStatus.count + 1));
           isVisited[nxtRow][nxtCol] = true;
         }
       }
     }
 
-    if (resultByPq.isEmpty()) return null;
-    return resultByPq.poll();
-  }
-  private static boolean isValidPoint(int row, int col, int N, int M) {
-    return row >= 0 && col >= 0 && row < N && col < M;
-  }
-  private static boolean isAliveEnemy(int[][] grid, int N, int M) {
+    if (pq.isEmpty()) return null;
 
-    for (int i = 0; i < N; i++) {
-      for (int j = 0; j < M; j++) {
+    return pq.poll().point;
+  }
+  private static boolean isValidPoint(int row, int col, int rowLen, int colLen) {
+    return row >= 0 && col >= 0 && row < rowLen && col < colLen;
+  }
+  private static int calcDistance(int arrowRowPos, int arrowColPos, int row, int col) {
+    return Math.abs(arrowRowPos - row) + Math.abs(arrowColPos - col);
+  }
+  private static int[][] getNextStepGrid(int[][] grid, int rowLen, int colLen) {
+
+    int[][] nextStepGrid = new int[rowLen][colLen];
+
+    for (int i = 0; i < rowLen - 1; i++) {
+      for (int j = 0; j < colLen; j++) {
+        nextStepGrid[i + 1][j] = grid[i][j];
+      }
+    }
+
+    return nextStepGrid;
+  }
+  private static boolean isContinue(int[][] grid, int rowLen, int colLen) {
+
+    for (int i = 0; i < rowLen; i++) {
+      for (int j = 0; j < colLen; j++) {
         if (grid[i][j] == 1) return true;
       }
     }
 
     return false;
   }
-}
-class ComparatorStatus implements Comparator<Status> {
-  @Override
-  public int compare(Status o1, Status o2) {
-    if (o1.row != o2.row) return o2.row - o1.row;
-    else return o1.col - o2.col;
+  private static int[][] copyIntGrid(int[][] grid, int rowLen, int colLen) {
+    int[][] copy = new int[rowLen][colLen];
+
+    for (int i = 0; i < rowLen; i++) {
+      for (int j = 0; j < colLen; j++) {
+        copy[i][j] = grid[i][j];
+      }
+    }
+
+    return copy;
   }
 }
-class Status{
-  int row;
-  int col;
+class RegistryInfoCompartor implements Comparator<RegistryInfo> {
+  @Override
+  public int compare(RegistryInfo o1, RegistryInfo o2) {
+    if (o1.distance != o2.distance) return o1.distance - o2.distance;
+    else {
+      return o1.point.col - o2.point.col;
+    }
+  }
+}
+class RegistryInfo {
+  Point point;
+  int distance;
+
+  RegistryInfo(Point point, int distance) {
+    this.point = point;
+    this.distance = distance;
+  }
+}
+class Status {
+  Point point;
   int count;
 
   Status(int row, int col, int count) {
+    this.point = new Point(row, col);
+    this.count = count;
+  }
+}
+class Point {
+  int row;
+  int col;
+
+  Point (int row, int col) {
     this.row = row;
     this.col = col;
-    this.count = count;
   }
 }
--- tier/gold/castle-defence/README.md
+++ tier/gold/castle-defence/README.md
@@ -613,3 +613,203 @@ class Node {
 
 적을 선택하는 기준은 거리가 D이하인 적들 중에 가장 가까운 적, 그리고 그런 적들이 여러 명일 경우에 가장 왼쪽에 있는 것이라고 언급함.
 
+**객체 지향적 풀이이지만 성능면에서 이전 풀이가 더 낫다.**
+
+```java
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+  private static int ENEMY_COUNT = 0;
+  private static int MAX;
+  private static final int[] DR = {-1, 0, 1, 0};
+  private static final int[] DC = {0, 1, 0, -1};
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+
+    int N = Integer.parseInt(st.nextToken());
+    int M = Integer.parseInt(st.nextToken());
+    int D = Integer.parseInt(st.nextToken());
+
+    int[][] grid = new int[N][M];
+
+    for (int i = 0; i < N; i++) {
+      st = new StringTokenizer(br.readLine());
+      for (int j = 0; j < M; j++) {
+        grid[i][j] = Integer.parseInt(st.nextToken());
+
+        if (grid[i][j] == 1) ENEMY_COUNT++;
+      }
+    }
+
+    System.out.println(simulationWithDFS(grid, N, M, D));
+  }
+  private static int simulationWithDFS(int[][] grid, int N, int M, int D) {
+
+    MAX = Integer.MIN_VALUE;
+    dfs(grid, new int[3], 0, M, 0, N, D);
+
+    return MAX;
+  }
+  private static void dfs(int[][] grid, int[] buffer, int ptr, int colLen, int size, int rowLen, int arrowRange) {
+
+    // 성능 개선 1 => 648ms -> 396ms
+    if (MAX == ENEMY_COUNT || MAX == (3 * rowLen)) return;
+
+    if (size == 3) {
+      int[][] copy = copyIntGrid(grid, rowLen, colLen);
+      MAX = Math.max(MAX, simulation(copy, buffer, rowLen, colLen, arrowRange));
+    } else if (ptr < colLen) {
+      buffer[size] = ptr;
+      dfs(grid, buffer, ptr + 1, colLen, size + 1, rowLen, arrowRange);
+      buffer[size] = 0;
+
+      dfs(grid, buffer, ptr + 1, colLen, size, rowLen, arrowRange);
+    }
+  }
+  private static int simulation(int[][] grid, int[] buffer, int rowLen, int colLen, int arrowRange) {
+
+    int result = 0;
+    while (isContinue(grid, rowLen, colLen)) {
+      Queue<Point> delEnemyPointBuffer = new ArrayDeque<>();
+      for (int i = 0; i < 3; i++) {
+        int arrowColPos = buffer[i];
+
+        Point delEnemyPoint = findEnemy(grid, rowLen, arrowColPos, rowLen, colLen, arrowRange);
+        if (delEnemyPoint != null) delEnemyPointBuffer.add(delEnemyPoint);
+      }
+
+      result += killEnemyCount(grid, delEnemyPointBuffer);
+
+      // 성능 개선 2 => 396ms -> 332ms
+      rowLen--;
+      //grid = getNextStepGrid(grid, rowLen, colLen);
+    }
+    return result;
+  }
+  private static int killEnemyCount(int[][] grid, Queue<Point> delEnemyPointBuffer) {
+
+    int killCount = 0;
+    while (!delEnemyPointBuffer.isEmpty()) {
+      Point delEnemyPoint = delEnemyPointBuffer.poll();
+
+      if (grid[delEnemyPoint.row][delEnemyPoint.col] == 1) {
+        killCount++;
+      }
+
+      grid[delEnemyPoint.row][delEnemyPoint.col] = 0;
+    }
+
+    return killCount;
+  }
+  private static Point findEnemy(int[][] grid, int arrowRowPos, int arrowColPos, int rowLen, int colLen, int arrowRange) {
+
+    boolean[][] isVisited = new boolean[rowLen][colLen];
+    Queue<Status> buffer = new ArrayDeque<>();
+    buffer.add(new Status(arrowRowPos - 1, arrowColPos, 1));
+    isVisited[arrowRowPos - 1][arrowColPos] = true;
+
+    Queue<RegistryInfo> pq = new PriorityQueue<>(new RegistryInfoCompartor());
+    while (!buffer.isEmpty()) {
+      Status curStatus = buffer.poll();
+
+      if (grid[curStatus.point.row][curStatus.point.col] == 1) {
+        pq.add(new RegistryInfo(curStatus. point, calcDistance(arrowRowPos, arrowColPos, curStatus.point.row, curStatus.point.col)));
+      }
+      if (curStatus.count == arrowRange) continue;
+
+      for (int i = 0; i < 4; i++) {
+        int nxtRow = curStatus.point.row + DR[i];
+        int nxtCol = curStatus.point.col + DC[i];
+
+        if (isValidPoint(nxtRow, nxtCol, rowLen, colLen) && !isVisited[nxtRow][nxtCol]) {
+          buffer.add(new Status(nxtRow, nxtCol, curStatus.count + 1));
+          isVisited[nxtRow][nxtCol] = true;
+        }
+      }
+    }
+
+    if (pq.isEmpty()) return null;
+
+    return pq.poll().point;
+  }
+  private static boolean isValidPoint(int row, int col, int rowLen, int colLen) {
+    return row >= 0 && col >= 0 && row < rowLen && col < colLen;
+  }
+  private static int calcDistance(int arrowRowPos, int arrowColPos, int row, int col) {
+    return Math.abs(arrowRowPos - row) + Math.abs(arrowColPos - col);
+  }
+  private static int[][] getNextStepGrid(int[][] grid, int rowLen, int colLen) {
+
+    int[][] nextStepGrid = new int[rowLen][colLen];
+
+    for (int i = 0; i < rowLen - 1; i++) {
+      for (int j = 0; j < colLen; j++) {
+        nextStepGrid[i + 1][j] = grid[i][j];
+      }
+    }
+
+    return nextStepGrid;
+  }
+  private static boolean isContinue(int[][] grid, int rowLen, int colLen) {
+
+    for (int i = 0; i < rowLen; i++) {
+      for (int j = 0; j < colLen; j++) {
+        if (grid[i][j] == 1) return true;
+      }
+    }
+
+    return false;
+  }
+  private static int[][] copyIntGrid(int[][] grid, int rowLen, int colLen) {
+    int[][] copy = new int[rowLen][colLen];
+
+    for (int i = 0; i < rowLen; i++) {
+      for (int j = 0; j < colLen; j++) {
+        copy[i][j] = grid[i][j];
+      }
+    }
+
+    return copy;
+  }
+}
+class RegistryInfoCompartor implements Comparator<RegistryInfo> {
+  @Override
+  public int compare(RegistryInfo o1, RegistryInfo o2) {
+    if (o1.distance != o2.distance) return o1.distance - o2.distance;
+    else {
+      return o1.point.col - o2.point.col;
+    }
+  }
+}
+class RegistryInfo {
+  Point point;
+  int distance;
+
+  RegistryInfo(Point point, int distance) {
+    this.point = point;
+    this.distance = distance;
+  }
+}
+class Status {
+  Point point;
+  int count;
+
+  Status(int row, int col, int count) {
+    this.point = new Point(row, col);
+    this.count = count;
+  }
+}
+class Point {
+  int row;
+  int col;
+
+  Point (int row, int col) {
+    this.row = row;
+    this.col = col;
+  }
+}
+```
