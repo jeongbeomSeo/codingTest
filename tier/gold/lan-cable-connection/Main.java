664195303d7124fc36bdb47428af47d486327b04
jeongbeomSeo <jeongbeom4693@gmail.com>
2024-01-29 20:03:37 +0900

    풀이: 랜선 연결 잘못된 풀이 컴퓨터와 처음에 연결하는 스위치는 포트가 -1 감소하지만 이후 스위치를 더 붙일 경우 스위치끼리 연결해야하기 때문에 -1이 더 감소된다. 또한 이런 논리되로라면 마지막 스위치는 사용 가능한 포트의 수의 감소량은 -1 밖에 되지 않는다.

--- tier/gold/lan-cable-connection/Main.java
+++ tier/gold/lan-cable-connection/Main.java
@@ -5,68 +5,62 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-  static int MAX_COM = 100001;
+  private static final int INF = Integer.MAX_VALUE;
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     StringTokenizer st;
 
     int N = Integer.parseInt(br.readLine());
 
-    Switch[] switches = new Switch[N + 1];
-    for (int i = 0; i < N + 1; i++) {
-      switches[i] = new Switch();
-    }
-
-    for (int i = 0; i < N; i++) {
+    // SwitchArray Info
+    // element: Number of Available ports, cost
+    int[][] switchArary = new int[N + 1][2];
+    for (int i = 1; i <= N; i++) {
       st = new StringTokenizer(br.readLine());
-      switches[i].port = Integer.parseInt(st.nextToken());
-      switches[i].cost = Integer.parseInt(st.nextToken());
+      switchArary[i][0] = Integer.parseInt(st.nextToken()) - 1;
+      switchArary[i][1] = Integer.parseInt(st.nextToken());
     }
 
-    Arrays.sort(switches);
+    int computerCount = Integer.parseInt(br.readLine());
 
-    for (int i = 1; i < N + 1; i++) {
-      System.out.println(switches[i].port + " " + switches[i].cost);
-    }
+    // 직접 연결
+    if (computerCount == 1) System.out.println(0);
+    else {
+      long[][] resultDpTable = queryDP(switchArary, computerCount, N);
 
-    int M = Integer.parseInt(br.readLine());
-
-    long[] dp = new long[MAX_COM];
-
-  }
-  static void dp(Switch[] switches, long[] dp, int N, int M) {
-
-    dp[1] = switches[1].cost;
-    dp[2] = switches[1].cost;
-
-    for (int i = 1; i <= switches[1].port; i++) {
-      dp[i] = switches[1].cost;
-      if (i == M) return;
-    }
-
-    int curPtr = switches[1].port + 1;
-    int curSwitch = 2;
-    while (curPtr < M) {
-      dp[curPtr] = Math.min(dp[curPtr - switches[curSwitch - 1].port] + switches[curSwitch].cost,
-              )
+      if (resultDpTable[N][computerCount] == INF) System.out.println(-1);
+      else System.out.println(resultDpTable[N][computerCount]);
     }
   }
+  private static long[][] queryDP(int[][] switchArray, int computerCount, int N) {
+    long[][] dpTable = initDpTable(N, computerCount);
 
-}
+    for (int i = 1; i <= N; i++) {
+      int port = switchArray[i][0];
+      int cost = switchArray[i][1];
 
-class Switch implements Comparable<Switch>{
-  int port;
-  int cost;
+      int j;
+      for (j = 1; j <= port && j <= computerCount; j++) {
+        dpTable[i][j] = Math.min(dpTable[i - 1][j], cost);
+      }
+      for (; j <= computerCount; j++) {
+        if (dpTable[i - 1][j - port] != INF) {
+          dpTable[i][j] = Math.min(dpTable[i - 1][j - port] + cost, dpTable[i - 1][j]);
+        }
+        else break;
+      }
+    }
 
-
-  Switch() {}
-  Switch(int port, int cost) {
-    this.port = port;
-    this.cost = cost;
+    return dpTable;
   }
+  private static long[][] initDpTable(int N, int computerCount) {
+    long[][] dpTable = new long[N + 1][computerCount + 1];
 
-  @Override
-  public int compareTo(Switch o) {
-    return this.cost - o.cost;
+    for (int i = 0; i < N + 1; i++) {
+      Arrays.fill(dpTable[i], INF);
+      dpTable[i][0] = 0;
+    }
+
+    return dpTable;
   }
 }
