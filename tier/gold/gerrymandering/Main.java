6ccdb2d9f95f33e165d3cfd5737c923cb03ba6d5
jeongbeomSeo <jeongbeom4693@gmail.com>
2024-01-29 17:18:23 +0900

    복습중: 게리맨더링

--- tier/gold/gerrymandering/Main.java
+++ tier/gold/gerrymandering/Main.java
@@ -1,151 +1,108 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-  static int MAX = 10001;
-
-  // 목표: 최솟값 설정
-  static int diffMin = MAX;
-  static int[] areaA, areaB;
-
+  private static final int INF = Integer.MAX_VALUE;
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     StringTokenizer st;
 
     int N = Integer.parseInt(br.readLine());
 
-    int[] population = new int[N + 1];
+    int[] sizeInDistrictArray = new int[N + 1];
     st = new StringTokenizer(br.readLine());
-    for (int i = 1; i < N + 1; i++) {
-      population[i] = Integer.parseInt(st.nextToken());
-    }
-
-    ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
-    for (int i = 0; i < N + 1; i++) {
-      graph.add(new ArrayList<>());
-    }
-
-    for (int i = 1; i < N + 1; i++) {
-      st = new StringTokenizer(br.readLine());
-      int connectionCount = Integer.parseInt(st.nextToken());
-
-      for (int j = 0; j < connectionCount; j++) {
-        graph.get(i).add(Integer.parseInt(st.nextToken()));
-      }
-    }
-
-    // A구역, B구역 배열 생성
-    areaA = new int[N];
-    areaB = new int[N];
-
-    // 팀나누기는 미리 한명 넣어놓고 시작
-    areaA[0] = 1;
-
-    // Logic
-
-    // 조합으로 인수 나누기 -> 조합 방식을 사용하고 싶다면 되도록이면 재귀 함수를 사용하는 것을 지향해야 할 것 같다.
-    // 문제는 재귀 함수로 사용하는 순간 함수 내에서 변수 초기화는 힘들다.
-
-    // 조합으로 나눠지는 경우 나올 때 마다 구역별로 방분 가능 여부 확인 (BFS)
-    // 방문 가능할 경우 인구수 차이 계산후 최솟값일 경우 갱신
-
-    combination(population, graph, 2, 1, 0, N);
-
-    if (diffMin == MAX) System.out.println(-1);
-    else System.out.println(diffMin);
-  }
-
-  static void combination(int[] population,  ArrayList<ArrayList<Integer>> graph , int ptr, int sizeA, int sizeB, int N) {
-
-    if (ptr == N + 1) {
-      if (sizeA != 0 && sizeB != 0) {
-        if (bfs(graph, sizeA, sizeB, N)) {
-          diffMin = Math.min(diffMin, calcDiff(population, sizeA, sizeB));
-        }
-      }
-    } else {
-      areaA[sizeA] = ptr;
-      combination(population, graph, ptr + 1, sizeA + 1, sizeB, N);
-      areaA[sizeA] = 0;
-
-
-      areaB[sizeB] = ptr;
-      combination(population, graph, ptr + 1, sizeA, sizeB + 1, N);
-      areaB[sizeB] = ptr;
-    }
-  }
-
-  // A구역 탐색 -> B구역 탐색 -> isVisited 1 ~ N까지 전부 ture일 경우 return true
-  static boolean bfs(ArrayList<ArrayList<Integer>> graph, int sizeA, int sizeB, int N) {
-
-    boolean[] isVisited = new boolean[N + 1];
-    Queue<Integer> q = new ArrayDeque<>();
-
-
-    // A구역 확인
-    q.add(areaA[0]);
-    isVisited[areaA[0]] = true;
-
-    while (!q.isEmpty()) {
-      int curNode = q.poll();
-
-      for (int i = 0; i < graph.get(curNode).size(); i++) {
-        int adjNode = graph.get(curNode).get(i);
-
-
-        for (int j = 1; j < sizeA; j++) {
-          if (adjNode == areaA[j] && !isVisited[areaA[j]]) {
-            q.add(adjNode);
-            isVisited[adjNode] = true;
-          }
-        }
-      }
-    }
-
-    // B구역 확인
-    q.add(areaB[0]);
-    isVisited[areaB[0]] = true;
-
-    while (!q.isEmpty()) {
-      int curNode = q.poll();
-
-      for (int i = 0; i < graph.get(curNode).size(); i++) {
-        int adjNode = graph.get(curNode).get(i);
-
-
-        for (int j = 1; j < sizeB; j++) {
-          if (adjNode == areaB[j] && !isVisited[areaB[j]]) {
-            q.add(adjNode);
-            isVisited[adjNode] = true;
-          }
-        }
-      }
-    }
-
-    // 1 ~ N번 노드 미방문 여부 확인
     for (int i = 1; i <= N; i++) {
-      if (isVisited[i] == false) return false;
+      sizeInDistrictArray[i] = Integer.parseInt(st.nextToken());
     }
-    return true;
+
+    List<Integer>[] graph = new List[N + 1];
+    for (int i = 0; i <= N; i++) {
+      graph[i] = new ArrayList<>();
+    }
+
+    for (int i = 1; i <= N; i++) {
+      st = new StringTokenizer(br.readLine());
+      int size = Integer.parseInt(st.nextToken());
+
+      for (int j = 0; j < size; j++) {
+        int nearIdx = Integer.parseInt(st.nextToken());
+        graph[i].add(nearIdx);
+        graph[nearIdx].add(i);
+      }
+    }
+
+    int result = gerrymandering(graph, sizeInDistrictArray, N);
+    System.out.println(result != INF ? result : -1);
   }
+  private static int gerrymandering(List<Integer>[] graph, int[] sizeInDistrictArray, int N) {
 
-  static int calcDiff(int[] population, int sizeA, int sizeB) {
-    int sumA = 0;
-    int sumB = 0;
+    int minSubtraction = INF;
+    for (int i = 1; i < (1 << N) - 1; i++) {
+      int groupASize = 0;
+      int groupBSize = 0;
+      boolean[] isGroupABoolArray = new boolean[N + 1];
+      for (int j = 0; j < N; j++) {
+        if ((i & (1 << j)) != 0) {
+          isGroupABoolArray[j + 1] = true;
+          groupASize++;
+        }
+        else groupBSize++;
+      }
 
-    for (int i = 0; i < sizeA; i++) {
-      sumA += population[areaA[i]];
+      if (!isValidGroup(graph, isGroupABoolArray, true, groupASize, N)) continue;
+      if (!isValidGroup(graph, isGroupABoolArray, false, groupBSize, N)) continue;
+
+      int groupACount = 0;
+      int groupBCount = 0;
+      for (int j = 1; j <= N; j++) {
+        if (isGroupABoolArray[j]) groupACount += sizeInDistrictArray[j];
+        else groupBCount += sizeInDistrictArray[j];
+      }
+
+      minSubtraction = Math.min(minSubtraction, Math.abs(groupACount - groupBCount));
     }
 
-    for (int i = 0; i < sizeB; i++) {
-      sumB += population[areaB[i]];
+    return minSubtraction;
+  }
+  private static boolean isValidGroup(List<Integer>[] graph, boolean[] isGroupABoolArray,
+                                      boolean checkGroup, int groupSize, int N) {
+    // If Group is A then checkGroup is true
+
+    int visitCount = 0;
+    boolean[] isVisited = new boolean[N + 1];
+    Queue<Integer> nodeIdxBuffer = new ArrayDeque<>();
+    int startNodeIdx = getStartNode(isGroupABoolArray, checkGroup, N);
+    nodeIdxBuffer.add(startNodeIdx);
+    isVisited[startNodeIdx] = true;
+    visitCount++;
+
+    if (visitCount == groupSize) return true;
+
+    while (!nodeIdxBuffer.isEmpty()) {
+      int curNodeIdx = nodeIdxBuffer.poll();
+
+      if (visitCount == groupSize) return true;
+
+      for (int i = 0; i < graph[curNodeIdx].size(); i++) {
+        int nearNodeIdx = graph[curNodeIdx].get(i);
+        if (!isVisited[nearNodeIdx] && isGroupABoolArray[nearNodeIdx] == checkGroup) {
+          nodeIdxBuffer.add(nearNodeIdx);
+          isVisited[nearNodeIdx] = true;
+          visitCount++;
+        }
+      }
     }
 
-    return Math.abs(sumA - sumB);
+    return visitCount == groupSize;
+  }
+  private static int getStartNode(boolean[] isGroupBoolArray, boolean checkGroup, int N) {
+    for (int i = 1; i < N + 1; i++) {
+      if (isGroupBoolArray[i] == checkGroup) return i;
+    }
+
+    System.out.println("Logick Error: getStartNode");
+    return -1;
   }
 }
--- tier/gold/gerrymandering/README.md
+++ tier/gold/gerrymandering/README.md
@@ -439,3 +439,116 @@ public class Main {
   }
 }
 ```
+
+**최종 코드**
+
+```java
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+  private static final int INF = Integer.MAX_VALUE;
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st;
+
+    int N = Integer.parseInt(br.readLine());
+
+    int[] sizeInDistrictArray = new int[N + 1];
+    st = new StringTokenizer(br.readLine());
+    for (int i = 1; i <= N; i++) {
+      sizeInDistrictArray[i] = Integer.parseInt(st.nextToken());
+    }
+
+    List<Integer>[] graph = new List[N + 1];
+    for (int i = 0; i <= N; i++) {
+      graph[i] = new ArrayList<>();
+    }
+
+    for (int i = 1; i <= N; i++) {
+      st = new StringTokenizer(br.readLine());
+      int size = Integer.parseInt(st.nextToken());
+
+      for (int j = 0; j < size; j++) {
+        int nearIdx = Integer.parseInt(st.nextToken());
+        graph[i].add(nearIdx);
+        graph[nearIdx].add(i);
+      }
+    }
+
+    int result = gerrymandering(graph, sizeInDistrictArray, N);
+    System.out.println(result != INF ? result : -1);
+  }
+  private static int gerrymandering(List<Integer>[] graph, int[] sizeInDistrictArray, int N) {
+
+    int minSubtraction = INF;
+    for (int i = 1; i < (1 << N) - 1; i++) {
+      int groupASize = 0;
+      int groupBSize = 0;
+      boolean[] isGroupABoolArray = new boolean[N + 1];
+      for (int j = 0; j < N; j++) {
+        if ((i & (1 << j)) != 0) {
+          isGroupABoolArray[j + 1] = true;
+          groupASize++;
+        }
+        else groupBSize++;
+      }
+
+      if (!isValidGroup(graph, isGroupABoolArray, true, groupASize, N)) continue;
+      if (!isValidGroup(graph, isGroupABoolArray, false, groupBSize, N)) continue;
+
+      int groupACount = 0;
+      int groupBCount = 0;
+      for (int j = 1; j <= N; j++) {
+        if (isGroupABoolArray[j]) groupACount += sizeInDistrictArray[j];
+        else groupBCount += sizeInDistrictArray[j];
+      }
+
+      minSubtraction = Math.min(minSubtraction, Math.abs(groupACount - groupBCount));
+    }
+
+    return minSubtraction;
+  }
+  private static boolean isValidGroup(List<Integer>[] graph, boolean[] isGroupABoolArray,
+                                      boolean checkGroup, int groupSize, int N) {
+    // If Group is A then checkGroup is true
+
+    int visitCount = 0;
+    boolean[] isVisited = new boolean[N + 1];
+    Queue<Integer> nodeIdxBuffer = new ArrayDeque<>();
+    int startNodeIdx = getStartNode(isGroupABoolArray, checkGroup, N);
+    nodeIdxBuffer.add(startNodeIdx);
+    isVisited[startNodeIdx] = true;
+    visitCount++;
+
+    if (visitCount == groupSize) return true;
+
+    while (!nodeIdxBuffer.isEmpty()) {
+      int curNodeIdx = nodeIdxBuffer.poll();
+
+      if (visitCount == groupSize) return true;
+
+      for (int i = 0; i < graph[curNodeIdx].size(); i++) {
+        int nearNodeIdx = graph[curNodeIdx].get(i);
+        if (!isVisited[nearNodeIdx] && isGroupABoolArray[nearNodeIdx] == checkGroup) {
+          nodeIdxBuffer.add(nearNodeIdx);
+          isVisited[nearNodeIdx] = true;
+          visitCount++;
+        }
+      }
+    }
+
+    return visitCount == groupSize;
+  }
+  private static int getStartNode(boolean[] isGroupBoolArray, boolean checkGroup, int N) {
+    for (int i = 1; i < N + 1; i++) {
+      if (isGroupBoolArray[i] == checkGroup) return i;
+    }
+
+    System.out.println("Logick Error: getStartNode");
+    return -1;
+  }
+}
+```
