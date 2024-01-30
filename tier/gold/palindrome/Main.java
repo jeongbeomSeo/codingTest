44df5645f1c57d8f14d32ae46daebe8afd2d0345
jeongbeomSeo <jeongbeom4693@gmail.com>
2024-01-30 16:42:29 +0900

    복습중: 팰린드롬

--- tier/gold/palindrome/Main.java
+++ tier/gold/palindrome/Main.java
@@ -8,40 +8,48 @@ public class Main {
     StringTokenizer st;
 
     int N = Integer.parseInt(br.readLine());
-
-    int[] nums = new int[N];
+    int[] numArray = new int[N + 1];
     st = new StringTokenizer(br.readLine());
-    for (int i = 0; i < N; i++) nums[i] = Integer.parseInt(st.nextToken());
+    for (int i = 1; i <= N; i++) {
+      numArray[i] = Integer.parseInt(st.nextToken());
+    }
 
-    boolean[][] is_pd = query_pd(nums, N);
+    boolean[][] dpTable = initDpTable(numArray, N);
+
+    updateDpTable(numArray, dpTable, N);
 
     int M = Integer.parseInt(br.readLine());
     for (int i = 0; i < M; i++) {
       st = new StringTokenizer(br.readLine());
-      int s = Integer.parseInt(st.nextToken()) - 1;
-      int e = Integer.parseInt(st.nextToken()) - 1;
 
-      bw.write((is_pd[s][e] ? 1 : 0) +"\n");
+      int start = Integer.parseInt(st.nextToken());
+      int end = Integer.parseInt(st.nextToken());
+
+      bw.write((dpTable[start][end] ? 1 : 0) + "\n");
     }
     bw.flush();
     bw.close();
   }
-  static boolean[][] query_pd(int[] nums, int N) {
+  private static void updateDpTable(int[] numArray, boolean[][] dpTable, int N) {
 
-    boolean[][] is_pd = new boolean[N][N];
-
-    for (int i = 0; i < N; i++) {
-      is_pd[i][i] = true;
-
-      if (i != N - 1 && nums[i] == nums[i + 1]) is_pd[i][i + 1] = true;
-    }
-
-    for (int i = 2; i <= N - 1; i++) {
-      for (int j = 0; j + i < N; j++) {
-        if (nums[j] == nums[j + i] && is_pd[j + 1][j + i - 1]) is_pd[j][j + i] = true;
+    for (int i = 3; i <= N; i++) {
+      for (int j = 1; j < i - 1; j++) {
+        if (numArray[i] == numArray[j] && dpTable[j + 1][i - 1]) {
+          dpTable[j][i] = true;
+        }
       }
     }
+  }
+  private static boolean[][] initDpTable(int[] numArray, int N) {
+    boolean[][] dpTable = new boolean[N + 1][N + 1];
 
-    return is_pd;
+    for (int i = 1; i <= N; i++) {
+      dpTable[i][i] = true;
+
+      if (numArray[i - 1] == numArray[i])
+        dpTable[i - 1][i] = true;
+    }
+
+    return dpTable;
   }
 }
--- tier/gold/palindrome/README.md
+++ tier/gold/palindrome/README.md
@@ -205,3 +205,63 @@ public class Main {
   }
 }
 ```
+
+**복습 풀이(AC)**
+
+```java
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    StringTokenizer st;
+
+    int N = Integer.parseInt(br.readLine());
+    int[] numArray = new int[N + 1];
+    st = new StringTokenizer(br.readLine());
+    for (int i = 1; i <= N; i++) {
+      numArray[i] = Integer.parseInt(st.nextToken());
+    }
+
+    boolean[][] dpTable = initDpTable(numArray, N);
+
+    updateDpTable(numArray, dpTable, N);
+
+    int M = Integer.parseInt(br.readLine());
+    for (int i = 0; i < M; i++) {
+      st = new StringTokenizer(br.readLine());
+
+      int start = Integer.parseInt(st.nextToken());
+      int end = Integer.parseInt(st.nextToken());
+
+      bw.write((dpTable[start][end] ? 1 : 0) + "\n");
+    }
+    bw.flush();
+    bw.close();
+  }
+  private static void updateDpTable(int[] numArray, boolean[][] dpTable, int N) {
+
+    for (int i = 3; i <= N; i++) {
+      for (int j = 1; j < i - 1; j++) {
+        if (numArray[i] == numArray[j] && dpTable[j + 1][i - 1]) {
+          dpTable[j][i] = true;
+        }
+      }
+    }
+  }
+  private static boolean[][] initDpTable(int[] numArray, int N) {
+    boolean[][] dpTable = new boolean[N + 1][N + 1];
+
+    for (int i = 1; i <= N; i++) {
+      dpTable[i][i] = true;
+
+      if (numArray[i - 1] == numArray[i])
+        dpTable[i - 1][i] = true;
+    }
+
+    return dpTable;
+  }
+}
+```
