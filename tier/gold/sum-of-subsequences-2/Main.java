b2721940a7178cbe366611a28414400626de1e6a
jeongbeomSeo <jeongbeom4693@gmail.com>
2024-02-02 19:12:49 +0900

    복습중: 부분수열의 합 2 부분수열이 붙어있는 즉, 연속되는 부분수열과 착각하여 문제를 풀었음. 연속되는 부분수열을 구할 때는 반복을 할 때 앞에서부터 무조건 넣는 경우로 시작하여 뒤에 숫자를 넣는경우에 새롭게 리스트에 추가되는 식으로 생각하면됨. 따라서, boolean[] 을 사용하여 앞의 숫자가 들어갔는지 안들어갔는지 체크할 필요 없음.

--- tier/gold/sum-of-subsequences-2/Main.java
+++ tier/gold/sum-of-subsequences-2/Main.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -14,52 +15,59 @@ public class Main {
     int S = Integer.parseInt(st.nextToken());
 
     int[] nums = new int[N];
+
     st = new StringTokenizer(br.readLine());
-    for (int i = 0; i < N; i++) nums[i] = Integer.parseInt(st.nextToken());
-
-    ArrayList<Integer> aSum = new ArrayList<>();
-    ArrayList<Integer> bSum = new ArrayList<>();
-
-    combination(aSum, nums, 0, 0, N / 2);
-    combination(bSum, nums, N / 2, 0, N);
-
-    Collections.sort(bSum);
-    int size = bSum.size();
-    long count = 0;
-    for (int i = 0; i < aSum.size(); i++) {
-      int target = S - aSum.get(i);
-
-      count += (upper_bound(bSum, 0 , size, target) - lower_bound(bSum, 0, size, target));
+    for (int i = 0; i < N; i++) {
+      nums[i] = Integer.parseInt(st.nextToken());
     }
-    if (S == 0) System.out.println(count - 1);
-    else System.out.println(count);
+
+    List<Integer> sumList = new ArrayList<>();
+
+    querySumList(sumList, nums, N);
+
+    Collections.sort(sumList);
+
+    int size = sumList.size();
+    int upperIdx = upper_bound(sumList, 0, size, S);
+    int lowerIdx = lower_bound(sumList, 0, size, S);
+
+    int result = upperIdx - lowerIdx;
+
+    System.out.println(result);
   }
-  static int upper_bound(ArrayList<Integer> bSum, int left, int right, int target) {
+  private static int upper_bound(List<Integer> sumList, int left, int right, int target) {
     while (left < right) {
       int mid = (left + right) / 2;
 
-      if (bSum.get(mid) <= target) left = mid + 1;
-      else right = mid;
+      if (sumList.get(mid) <= target) {
+        left = mid + 1;
+      } else {
+        right = mid;
+      }
     }
     return left;
   }
-  static int lower_bound(ArrayList<Integer> bSum, int left, int right, int target) {
+  private static int lower_bound(List<Integer> sumList, int left, int right, int target) {
     while (left < right) {
       int mid = (left + right) / 2;
 
-      if (bSum.get(mid) < target) left = mid + 1;
-      else right = mid;
+      if (sumList.get(mid) < target) {
+        left = mid + 1;
+      } else {
+        right = mid;
+      }
     }
     return left;
   }
-  static void combination(ArrayList<Integer> sumList, int[] nums, int ptr, int sum, int size) {
+  private static void querySumList(List<Integer> sumList, int[] nums, int size) {
 
-    if (ptr == size) {
+    for (int i = 0; i < size; i++) {
+      int sum = nums[i];
       sumList.add(sum);
-    }
-    else {
-      combination(sumList, nums, ptr + 1, sum + nums[ptr], size);
-      combination(sumList, nums, ptr + 1, sum, size);
+      for (int j = i + 1; j < size; j++) {
+        sum += nums[j];
+        sumList.add(sum);
+      }
     }
   }
 }
