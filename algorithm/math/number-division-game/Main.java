4980218a5ea023930159b09ca1afb4da20ec9a35
jeongbeomSeo <jeongbeom4693@gmail.com>
2024-02-07 16:11:59 +0900

    최종 복습: 수 나누기 게임

--- algorithm/math/number-division-game/Main.java
+++ algorithm/math/number-division-game/Main.java
@@ -2,47 +2,41 @@ import java.io.*;
 import java.util.StringTokenizer;
 
 public class Main {
-  static int N;
-  static int MAX = 1000001;
+  private static final int MAX_SIZE = 1000001;
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    StringTokenizer st;
     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    StringTokenizer st;
 
-    N = Integer.parseInt(br.readLine());
+    int N = Integer.parseInt(br.readLine());
+    int[] nums = new int[N];
+    boolean[] numBoolArray = new boolean[MAX_SIZE];
+    int[] resultArray = new int[MAX_SIZE];
 
-    int[] array = new int[N + 1];
-    int[] isUsingNumberIdx = new int[MAX];
     st = new StringTokenizer(br.readLine());
     for (int i = 0; i < N; i++) {
       int num = Integer.parseInt(st.nextToken());
 
-      array[i + 1] = num;
-      isUsingNumberIdx[num] = i + 1;
+      nums[i] = num;
+      numBoolArray[num] = true;
     }
 
-    int[] result = queryResult(array, isUsingNumberIdx);
-
-    for (int i = 1; i < N + 1; i++) {
-      bw.write(result[i] + " ");
-    }
-    bw.flush();
-    bw.close();
-  }
-  private static int[] queryResult(int[] array, int[] isUsingNumberIdx) {
-
-    int[] result = new int[N + 1];
-
-    for (int i = 1; i < N + 1; i++) {
-      int num = array[i];
-
-      for (int j = num * 2; j < MAX; j += num) {
-        if (isUsingNumberIdx[j] != 0) {
-          result[i]++;
-          result[isUsingNumberIdx[j]]--;
+    for (int i = 0; i < N; i++) {
+      int num = nums[i];
+      for (int j = num + num; j < MAX_SIZE; j += num) {
+        if (numBoolArray[j]) {
+          resultArray[j]--;
+          resultArray[num]++;
         }
       }
     }
-    return result;
+
+    for (int i = 0; i < N - 1; i++) {
+      bw.write(resultArray[nums[i]] + " ");
+    }
+    bw.write(resultArray[nums[N - 1]] + "");
+
+    bw.flush();
+    bw.close();
   }
 }
