8c73bf2ea9b3624226a9b85e21a7aae92f1df7fe
jeongbeomSeo <jeongbeom4693@gmail.com>
2024-02-13 17:22:59 +0900

    최종 복습: 하노이탑 K (long)Math.pow(2, N - 1) - 1;로 수정을 하니깐 정답이 나왔음. 정밀도 측면에서는 (long)(Math.pow(2, N - 1) - 1);이 더 높을 것이라고 예상이 되나 해당 문제는 정확히 옮긴 횟수를 공식으로 계산한 뒤에 1을 빼주는 것이 명확하므로 이와 같이 나온 것으로 추측. 명확하게 차이가 생기는 이유는 아직 모르겠음.

--- algorithm/recursive-function/hanoi-top-k/Main.java
+++ algorithm/recursive-function/hanoi-top-k/Main.java
@@ -4,39 +4,29 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int N;
-    static long K;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        N = Integer.parseInt(st.nextToken());
-        K = Long.parseLong(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        long K = Long.parseLong(st.nextToken());
 
-        int[] result = recursive(N, 1, 3, K);
-
-        System.out.println(result[0] + " " + result[1]);
+        query(N, K, 1, 3);
     }
-    private static int[] recursive(int N, int start, int end, long remain) {
+    private static void query(int N, long K, int start, int end) {
 
-        long curCount = (long)Math.pow(2, N) - 1;
-        int[] result = null;
-        if (remain - curCount > 0) {
-            // N개 start -> end
-            remain -= curCount;
-
-            // N + 1블럭 start -> 6 - start - end로 이동
-            remain--;
-
-            if (remain == 0) {
-                return new int[]{start, 6 - start - end};
-            } else {
-                result = recursive(N, end, 6 - start - end, remain);
-            }
-        } else {
-            result = recursive(N - 1, start, 6 - start - end, remain);
+        if (K <= (long)Math.pow(2, N - 1) - 1) {
+            query(N - 1, K, start, 6 - start - end);
+            return;
         }
 
-        return result;
+        K -= (long)Math.pow(2, N - 1) - 1;
+
+        if (K == 1) {
+            System.out.println(start + " " + end);
+            return;
+        }
+
+        query(N - 1, K - 1, 6 - start - end, end);
     }
 }
