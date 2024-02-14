f541ebdd957fb3cac5a4932260a8655abc03bed8
jeongbeomSeo <jeongbeom4693@gmail.com>
2024-02-14 16:04:40 +0900

    최종 복습: 2048-easy dfs 풀이

--- algorithm/simulation/2048-easy/Main.java
+++ algorithm/simulation/2048-easy/Main.java
@@ -1,13 +1,11 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
 import java.util.Arrays;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-  private static final int MINUS_INF = Integer.MIN_VALUE;
+  private static int result;
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     StringTokenizer st;
@@ -22,194 +20,166 @@ public class Main {
       }
     }
 
-    System.out.println(simulation(grid, N));
+    System.out.println(query(grid, N));
   }
-  private static int simulation(int[][] grid, int size) {
+  private static int query(int[][] grid, int N) {
+    result = 0;
 
-    Queue<Board> q = new ArrayDeque<>();
-    q.add(new Board(grid, 0));
+    dfs(grid, N, 0);
 
-    int max = MINUS_INF;
+    return result;
+  }
+  private static void dfs(int[][] curGrid, int N, int count) {
 
-    while (!q.isEmpty()) {
-      Board board = q.poll();
-
-      if (board.count == 5) {
-        max = Math.max(max, getMaxValue(board.grid, size));
-        continue;
-      }
-
-      for (int i = 0; i < 4; i++) {
-        Board nextBoard = getNextBoard(board, i, size);
-
-        if (!isEqualGrid(board.grid, nextBoard.grid, size)) {
-          q.add(nextBoard);
-        }
-      }
+    if (count == 5) {
+      result = Math.max(result, getBiggestNumber(curGrid, N));
+      return;
     }
 
-    return max != MINUS_INF ? max : getMaxValue(grid, size);
-  }
-  private static boolean isEqualGrid(int[][] grid, int[][] nextGird, int size) {
+    for (int i = 0; i < 4; i++) {
+      int[][] newGrid = copy(curGrid, N);
 
-    for (int i = 0; i < size; i++) {
-      for (int j = 0; j < size; j++) {
-        if (grid[i][j] != nextGird[i][j]) return false;
-      }
+      moveGrid(newGrid, N, i);
+      dfs(newGrid, N, count + 1);
     }
-
-    return true;
   }
-  private static Board getNextBoard(Board board, int direction, int size) {
-
-    int[][] nextGrid = getNextGrid(board.grid, direction, size);
-
-    return new Board(nextGrid, board.count + 1);
-  }
-  private static int[][] getNextGrid(int[][] grid, int direction, int size) {
-
-    int[][] nextGrid = copyGrid(grid, size);
+  private static void moveGrid(int[][] grid, int N, int direction) {
 
     switch (direction) {
       case 0:
-        moveUp(nextGrid, size);
+        moveLeft(grid, N);
         break;
       case 1:
-        moveRight(nextGrid, size);
+        moveUp(grid, N);
         break;
       case 2:
-        moveDown(nextGrid, size);
+        moveRight(grid, N);
         break;
       case 3:
-        moveLeft(nextGrid, size);
+        moveDown(grid, N);
         break;
     }
-
-    return nextGrid;
   }
-  private static void moveUp(int[][] grid, int size) {
+  private static void moveLeft(int[][] grid, int N) {
+    for (int row = 0; row < N; row++) {
+      for (int col = 0; col < N - 1; col++) {
+        if (grid[row][col] == 0) {
+          int nxtCol = col + 1;
+          while (nxtCol < N && grid[row][nxtCol] == 0) nxtCol++;
 
-    boolean[][] isCrush = new boolean[size][size];
+          if (nxtCol < N) {
+            grid[row][col] = grid[row][nxtCol];
+            grid[row][nxtCol] = 0;
+          }
+        }
 
-    for (int j = 0; j < size; j++) {
-      for (int i = 1; i < size; i++) {
-        if (grid[i][j] != 0) {
-          int prevRow = i - 1;
+        if (grid[row][col] != 0) {
+          int nxtCol = col + 1;
+          while (nxtCol < N && grid[row][nxtCol] == 0) nxtCol++;
 
-          while (prevRow >= 0 && grid[prevRow][j] == 0) prevRow--;
-
-          if (prevRow == -1) {
-            grid[prevRow + 1][j] = grid[i][j];
-            grid[i][j] = 0;
-          } else if (grid[prevRow][j] == grid[i][j] && !isCrush[prevRow][j]) {
-            grid[prevRow][j] *= 2;
-            grid[i][j] = 0;
-            isCrush[prevRow][j] = true;
-          } else if (prevRow + 1 != i) {
-            grid[prevRow + 1][j] = grid[i][j];
-            grid[i][j] = 0;
+          if (nxtCol < N && grid[row][col] == grid[row][nxtCol]) {
+            grid[row][col] *= 2;
+            grid[row][nxtCol] = 0;
           }
         }
       }
     }
   }
-  private static void moveDown(int[][] grid, int size) {
+  private static void moveRight(int[][] grid, int N) {
+    for (int row = 0; row < N; row++) {
+      for (int col = N - 1; col > 0; col--) {
+        if (grid[row][col] == 0) {
+          int nxtCol = col - 1;
+          while (nxtCol >= 0 && grid[row][nxtCol] == 0) nxtCol--;
 
-    boolean[][] isCrush = new boolean[size][size];
+          if (nxtCol >= 0) {
+            grid[row][col] = grid[row][nxtCol];
+            grid[row][nxtCol] = 0;
+          }
+        }
 
-    for (int j = 0; j < size; j++) {
-      for (int i = size - 2; i >= 0; i--) {
-        if (grid[i][j] != 0) {
-          int prevRow = i + 1;
-          while (prevRow <= size - 1 && grid[prevRow][j] == 0) prevRow++;
+        if (grid[row][col] != 0) {
+          int nxtCol = col - 1;
+          while (nxtCol >= 0 && grid[row][nxtCol] == 0) nxtCol--;
 
-          if (prevRow == size) {
-            grid[size - 1][j] = grid[i][j];
-            grid[i][j] = 0;
-          } else if (grid[prevRow][j] == grid[i][j] && !isCrush[prevRow][j]) {
-            grid[prevRow][j] *= 2;
-            grid[i][j] = 0;
-            isCrush[prevRow][j] = true;
-          } else if (prevRow - 1 != i) {
-            grid[prevRow - 1][j] = grid[i][j];
-            grid[i][j] = 0;
+          if (nxtCol >= 0 && grid[row][col] == grid[row][nxtCol]) {
+            grid[row][col] *= 2;
+            grid[row][nxtCol] = 0;
           }
         }
       }
     }
   }
-  private static void moveRight(int[][] grid, int size) {
+  private static void moveDown(int[][] grid, int N) {
+    for (int col = 0; col < N; col++) {
+      for (int row = N - 1; row > 0; row--) {
+        if (grid[row][col] == 0) {
+          int nxtRow = row - 1;
+          while (nxtRow >= 0 && grid[nxtRow][col] == 0) nxtRow--;
 
-    boolean[][] isCrush = new boolean[size][size];
+          if (nxtRow >= 0) {
+            grid[row][col] = grid[nxtRow][col];
+            grid[nxtRow][col] = 0;
+          }
+        }
 
-    for (int i = 0; i < size; i++) {
-      for (int j = size - 2; j >= 0; j--) {
-        int prevCol = j + 1;
-        while (prevCol <= size - 1 && grid[i][prevCol] == 0) prevCol++;
+        if (grid[row][col] != 0) {
+          int nxtRow = row - 1;
+          while (nxtRow >= 0 && grid[nxtRow][col] == 0) nxtRow--;
 
-        if (prevCol == size) {
-          grid[i][prevCol - 1] = grid[i][j];
-          grid[i][j] = 0;
-        } else if (grid[i][prevCol] == grid[i][j] && !isCrush[i][prevCol]) {
-          grid[i][prevCol] *= 2;
-          grid[i][j] = 0;
-          isCrush[i][prevCol] = true;
-        } else if (prevCol - 1 != j) {
-          grid[i][prevCol - 1] = grid[i][j];
-          grid[i][j] = 0;
+          if (nxtRow >= 0 && grid[row][col] == grid[nxtRow][col]) {
+            grid[row][col] *= 2;
+            grid[nxtRow][col] = 0;
+          }
         }
       }
     }
   }
-  private static void moveLeft(int[][] grid, int size) {
+  private static void moveUp(int[][] grid, int N) {
+    for (int col = 0; col < N; col++) {
+      for (int row = 0; row < N - 1; row++) {
+        // 빈 공간일 경우(숫자 0) row가 큰 공간 중에 숫자가 0이 아닌 곳 탐색 후 있다면 옮겨주기
+        if (grid[row][col] == 0) {
+          int nxtRow = row + 1;
+          while (nxtRow < N && grid[nxtRow][col] == 0) nxtRow++;
 
-    boolean[][] isCrush = new boolean[size][size];
-    for (int i = 0; i < size; i++) {
-      for (int j = 1; j < size; j++) {
-        int prevCol = j - 1;
-        while (prevCol >= 0 && grid[i][prevCol] == 0) prevCol--;
+          if (nxtRow < N) {
+            grid[row][col] = grid[nxtRow][col];
+            grid[nxtRow][col] = 0;
+          }
+        }
+        // 현재 숫자가 0이 아닐 경우 row가 큰 공간 탐색 하면서 가장 먼저 나온 0이 아닌 숫자와 비교 만약 같다면 합쳐주기
+        if (grid[row][col] != 0) {
+          int nxtRow = row + 1;
+          while (nxtRow < N && grid[nxtRow][col] == 0) nxtRow++;
 
-        if (prevCol == -1) {
-          grid[i][prevCol + 1] = grid[i][j];
-          grid[i][j] = 0;
-        } else if (grid[i][prevCol] == grid[i][j] && !isCrush[i][prevCol]) {
-          grid[i][prevCol] *= 2;
-          grid[i][j] = 0;
-          isCrush[i][prevCol] = true;
-        } else if (prevCol + 1 != j) {
-          grid[i][prevCol + 1] = grid[i][j];
-          grid[i][j] = 0;
+          if (nxtRow < N && grid[row][col] == grid[nxtRow][col]) {
+            grid[row][col] *= 2;
+            grid[nxtRow][col] = 0;
+          }
         }
       }
     }
   }
-  private static int getMaxValue(int[][] grid, int size) {
-    int max = MINUS_INF;
+  private static int[][] copy(int[][] grid, int N) {
+    int[][] copy = new int[N][N];
 
-    for (int i = 0; i < size; i++) {
-      for (int j = 0; j < size; j++) {
-        max = Math.max(max, grid[i][j]);
-      }
-    }
-
-    return max;
-  }
-  private static int[][] copyGrid(int[][] grid, int size) {
-
-    int[][] copy = new int[size][size];
-    for (int i = 0; i < size; i++) {
-      copy[i] = Arrays.copyOf(grid[i], size);
+    for (int i = 0; i < N; i++) {
+      copy[i] = Arrays.copyOf(grid[i], N);
     }
 
     return copy;
   }
-}
-class Board {
-  int[][] grid;
-  int count;
+  private static int getBiggestNumber(int[][] grid, int N) {
 
-  Board(int[][] grid, int count) {
-    this.grid = grid;
-    this.count = count;
+    int num = 0;
+
+    for (int i = 0; i < N; i++) {
+      for (int j = 0; j < N; j++) {
+        num = Math.max(num, grid[i][j]);
+      }
+    }
+
+    return num;
   }
 }
