ef9012bcb36165e8488894334cb88f64caae66bd
jeongbeomSeo <jeongbeom4693@gmail.com>
2024-02-26 20:08:47 +0900

    최종 복습: 버블 소트

--- algorithm/sort/bubble-sort/Main.java
+++ algorithm/sort/bubble-sort/Main.java
@@ -28,34 +28,34 @@ public class Main {
     int size = getSize(N);
     int[] segTable = new int[size];
 
-    inputSegTable(segTable, 1, 0, N, nums[0].idx);
-
     long count = 0L;
-    for (int i = 1; i < N; i++) {
-      count += querySegTable(segTable, 1, 0, N, nums[i].idx, N);
-      inputSegTable(segTable, 1, 0, N, nums[i].idx);
+    for (int i = 0; i < N; i++) {
+      count += querySegTable(segTable, 1, 0, N - 1, nums[i].idx + 1, N - 1);
+      inputSegTable(segTable, 1, 0, N - 1, nums[i].idx);
     }
 
     return count;
   }
-  private static void inputSegTable(int[] segTable, int node, int start, int end, int idx) {
-    if (idx < start || end < idx) return;
-    else if (start == end) {
-      segTable[node]++;
+  private static void inputSegTable(int[] segTable, int node, int left, int right, int idx) {
+    if (idx < left || right < idx) return;
+    else if (left == right) {
+      segTable[node] = 1;
       return;
     }
 
-    inputSegTable(segTable, 2 * node, start, (start + end) / 2, idx);
-    inputSegTable(segTable, 2 * node + 1, (start + end) / 2 + 1, end, idx);
+    int mid = (left + right) / 2;
+    inputSegTable(segTable, 2 * node, left, mid, idx);
+    inputSegTable(segTable, 2 * node + 1, mid + 1, right, idx);
 
     segTable[node] = segTable[2 * node] + segTable[2 * node + 1];
   }
-  private static int querySegTable(int[] segTable, int node, int start, int end, int left, int right) {
-    if (right < start || end < left) return 0;
+  private static int querySegTable(int[] segTable, int node, int left, int right, int start, int end) {
+    if (end < left || right < start) return 0;
     else if (start <= left && right <= end) return segTable[node];
 
-    int leftResult = querySegTable(segTable, 2 * node, start, (start + end) / 2, left, right);
-    int rightResult = querySegTable(segTable, 2 * node + 1, (start + end) / 2 + 1, end, left, right);
+    int mid = (left + right) / 2;
+    int leftResult = querySegTable(segTable, 2 * node, left, mid, start, end);
+    int rightResult = querySegTable(segTable, 2 * node + 1, mid + 1, right, start, end);
 
     return leftResult + rightResult;
   }
@@ -77,9 +77,6 @@ class Number implements Comparable<Number>{
 
   @Override
   public int compareTo(Number o) {
-    if (this.num - o.num != 0) return this.num - o.num;
-    else {
-      return this.idx - o.idx;
-    }
+    return this.num - o.num;
   }
 }
